92467b737689e191b505e0d61e42f08fb06cf747
yuliy <aivyulik@gmail.com>
2011-12-01 01:07:59 +0400

    Solved 1260

--- timus/1260/main.cpp
+++ timus/1260/main.cpp
@@ -9,6 +9,20 @@
 using namespace std;
 
 int main() {
+    int s[60];
+    s[1] = 1;
+    s[2] = 1;
+    s[3] = 2;
+    s[4] = 4;
+    s[5] = 6;
+    s[6] = 9;
+    s[7] = 14;
 
+    int N;
+    scanf("%d", &N);
+    for (int i = 7; i <= N; ++i)
+        s[i] = s[i-1] + s[i-2] - s[i-5];
+
+    printf("%d\n", s[N]);
     return 0;
 }
