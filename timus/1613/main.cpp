24b376feeb0402a471b3efb6a7d5bbe29ad2d2d6
yuliy <aivyulik@gmail.com>
2011-12-04 00:38:39 +0400

    1613 solution

--- timus/1613/main.cpp
+++ timus/1613/main.cpp
@@ -8,7 +8,42 @@
 #include <algorithm>
 using namespace std;
 
-int main() {
+int N;
+int Q;
 
+typedef set<int> TCities;
+typedef map<int, TCities > TPeople2Cities;
+TPeople2Cities dic;
+
+static bool Check(int l, int r, int x) {
+    TPeople2Cities::const_iterator pciter = dic.find(x);
+    if (pciter == dic.end())
+        return false;
+
+    const TCities &cities = pciter->second;
+    TCities::const_iterator citer1 = cities.lower_bound(l);
+    TCities::const_iterator citer2 = cities.upper_bound(r);
+    return citer1 != citer2;
+}
+
+int main() {
+    scanf("%d\n", &N);
+    for (int i = 1; i <= N; ++i) {
+        int tmp;
+        scanf("%d", &tmp);
+        dic[tmp].insert(i);
+    }
+
+    scanf("%d\n", &Q);
+    for (int i = 1; i <= Q; ++i) {
+        int l, r, x;
+        scanf("%d %d %d\n", &l, &r, &x);
+        if (Check(l, r, x))
+            printf("1");
+        else
+            printf("0");
+    }
+
+    printf("\n");
     return 0;
 }
