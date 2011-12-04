55c07ab5c3528fac6a2224dabfe1a1425eb07f46
yuliy <aivyulik@gmail.com>
2011-12-04 23:53:38 +0400

    1126 solution

--- timus/1126/main.cpp
+++ timus/1126/main.cpp
@@ -8,7 +8,38 @@
 #include <algorithm>
 using namespace std;
 
+int M, N;
+typedef map<int, int, greater<int> > TNum2Count;
+TNum2Count dic;
+
 int main() {
+    scanf("%d\n", &M);
+
+    vector<int> v;
+    for (;;) {
+        int tmp;
+        scanf("%d\n", &tmp);
+        if (tmp == -1)
+            break;
+        v.push_back(tmp);
+    }
+    N = v.size();
+
+    for (int i = 0; i < M; ++i)
+        ++dic[v[i]];
+
+    for (int i = M; i <= N; ++i) {
+        TNum2Count::iterator maxIter = dic.begin();
+        printf("%d\n", maxIter->first);
+        if (dic[v[i-M]] > 1) {
+            --dic[v[i-M]];
+        } else {
+            TNum2Count::iterator it = dic.find(v[i-M]);
+            dic.erase(it);
+        }
+
+        ++dic[v[i]];
+    }
 
     return 0;
 }
