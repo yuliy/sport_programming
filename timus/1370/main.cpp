8afdeb08c897edb8dc3d5df89b92db73dc1e9633
yuliy <aivyulik@gmail.com>
2011-12-14 12:51:43 +0400

    1370 solution

--- timus/1370/main.cpp
+++ timus/1370/main.cpp
@@ -9,6 +9,21 @@
 using namespace std;
 
 int main() {
+    int N, M;
+    scanf("%d %d", &N, &M);
+
+    vector<int> v(N);
+    for (int i = 0; i < N; ++i)
+        scanf("%d", &v[i]);
+
+    const int start = M % N;
+    for (int i = 0; i < 10; ++i) {
+        int idx = start + i;
+        if (idx >= N)
+            idx -= N;
+        printf("%d", v[idx]);
+    }
+    printf("\n");
 
     return 0;
 }
