b32297b1b9b8dbb29d1de73d81c5e3f1189f3d58
Yuliy Ayvazyan <yuliy@yuliy-laptop.(none)>
2011-12-14 18:30:43 +0400

    1683 solution

--- timus/1683/main.cpp
+++ timus/1683/main.cpp
@@ -9,6 +9,35 @@
 using namespace std;
 
 int main() {
+    int N;
+    scanf("%d", &N);
+
+    if (N == 1) {
+        cout << 0 << endl;
+        return 0;
+    }
+
+    int n = N;
+    int res = 0;
+    vector<int> steps;
+    for (;;) {
+        ++res;
+        int nextn = 0;
+        if (n % 2)
+            ++nextn;
+        const int step = n / 2;
+        steps.push_back(step);
+        nextn += step;
+        n = nextn;
+        if (n == 1)
+            break;
+    }
+
+    printf("%d\n", res);
+    for (vector<int>::const_iterator iter = steps.begin(), end = steps.end(); iter != end; ++iter) {
+        printf("%d ", *iter);
+    }
+    printf("\n");
 
     return 0;
 }
