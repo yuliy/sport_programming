0ff4574dbd21035fb140f271cbd3ee59ddf821f3
Yuliy Ayvazyan <yuliy@yuliy-laptop.(none)>
2011-12-14 19:28:39 +0400

    1110 in progress

--- timus/1110/main.cpp
+++ timus/1110/main.cpp
@@ -8,7 +8,19 @@
 #include <algorithm>
 using namespace std;
 
+const int MAX_POW = 1000;
+const int MAX_X = 1000;
+int POW[MAX_X][MAX_POW];
+
+int N, M, Y;
+
+static void Init() {
+    //
+}
+
 int main() {
+    scanf("%d %d %d", &N, &M, &Y);
+    Init();
 
     return 0;
 }
