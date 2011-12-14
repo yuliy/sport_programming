79022642af680662ed3202fa669ac64a57befad4
yuliy <aivyulik@gmail.com>
2011-12-15 01:25:13 +0400

    1110 solution

--- timus/1110/main.cpp
+++ timus/1110/main.cpp
@@ -15,12 +15,30 @@ int POW[MAX_X][MAX_POW];
 int N, M, Y;
 
 static void Init() {
-    //
+    for (int i = 0; i < M; ++i)
+        POW[i][0] = 1;
+    for (int n = 1; n <= N; ++n) {
+        for (int i = 0; i < M; ++i) {
+            POW[i][n] = POW[i][n-1] * i;
+            POW[i][n] %= M;
+        }
+    }
 }
 
 int main() {
     scanf("%d %d %d", &N, &M, &Y);
     Init();
+
+    bool nosolution = true;
+    for (int i = 0; i < M; ++i) {
+        if (POW[i][N] == Y) {
+            printf("%d ", i);
+            nosolution = false;
+        }    
+    }
+    if (nosolution)
+        printf("-1");
+    printf("\n");
 
     return 0;
 }
