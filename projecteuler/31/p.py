9440072ee82c04c2c1814f552a4a8288ab9cb5c1
Yuliy Ayvazyan <yuliy@yuliy-laptop.(none)>
2012-01-19 21:30:06 +0400

    @

---
+++ projecteuler/31/p.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+Values = [
+    1,
+    2,
+    5,
+    10,
+    20,
+    50,
+    100,
+    200
+]
+
+ValuesCnt = 8
+
+def main():
+    d = []
+    for i in xrange(0, 251):
+        d.append(0)
+    d[0] = d[1] = 1
+
+    for v in xrange(2, 201):
+        for coin in Values:
+            if (v - coin) >= 1:
+                d[v] += d[v-coin]
+
+    for i in xrange(0, 251):
+        print i, d[i]
+
+if __name__ == '__main__':
+    main()
