5b32fc0e84ba217ffc9eeb42808110ba2f512c24
Yuliy Ayvazyan <yuliy@yuliy-laptop.(none)>
2012-01-25 18:29:00 +0400

    Problem #39 in progress

--- projecteuler/039/main.cpp
+++ projecteuler/039/main.cpp
@@ -8,6 +8,18 @@
 #include <algorithm>
 using namespace std;
 
+static bool IsRightTriangle(int a, int b, int c) {
+    vector<int> v(3);
+    v[0] = a;
+    v[1] = b;
+    v[3] = c;
+
+    sort(v.begin(), v.end());
+
+    return (sqr(v[0]) + sqr(v[1]) == sqr(v[2]));
+        
+}
+
 int main() {
 
     return 0;
