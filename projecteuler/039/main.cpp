65632395097d79babbcc55dd43e2cdedfa1119d0
Yuliy Ayvazyan <yuliy@yuliy-laptop.(none)>
2012-01-25 19:31:51 +0400

    Problem #39 solution

--- projecteuler/039/main.cpp
+++ projecteuler/039/main.cpp
@@ -6,21 +6,55 @@
 #include <set>
 #include <list>
 #include <algorithm>
+#include <cmath>
 using namespace std;
 
 static bool IsRightTriangle(int a, int b, int c) {
+    return a*a + b*b == c*c;
+    /*
     vector<int> v(3);
     v[0] = a;
     v[1] = b;
-    v[3] = c;
+    v[2] = c;
 
     sort(v.begin(), v.end());
 
-    return (sqr(v[0]) + sqr(v[1]) == sqr(v[2]));
-        
+    return (v[0]*v[0] + v[1]*v[1]) == (v[2]*v[2]);
+    */
+}
+
+static int SolutionsCount(int P) {
+    int res = 0;
+    for (int a = 1; a <= (P-2); ++a) {
+        for (int b = a; b <= (P-a-1); ++b) {
+            const int c = P-a-b;
+            if (c < b)
+                continue;
+            const bool rt = IsRightTriangle(a, b, c);
+            //cout << P << "\t" << a << "\t" << b << "\t" << "\t" << c << "\trt=" << rt << endl;
+            if (rt)
+                ++res;
+        }
+    }
+    return res;
 }
 
 int main() {
+    int maxSols = 0;
+    int maxP = 0;
+
+    for (int p = 3; p <= 1000; ++p) {
+        cout << "\tp=" << p << endl;
+
+        const int curSols = SolutionsCount(p);
+        if (curSols >= maxSols) {
+            maxSols = curSols;
+            maxP = p;
+        }
+    }
+
+    cout << "maxSols=" << maxSols << endl
+         << "maxP=" << maxP << endl;
 
     return 0;
 }
