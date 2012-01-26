28d71fc260d4c52a4b7c94d63f04050e19051b22
yuliy <aivyulik@gmail.com>
2012-01-26 20:46:15 +0400

    Problem #33 solution

--- projecteuler/033/main.cpp
+++ projecteuler/033/main.cpp
@@ -9,6 +9,16 @@
 using namespace std;
 
 int main() {
-
+    for (int i = 0; i < 10; ++i) {
+        for (int j = 0; j < 10; ++j) {
+            for (int k = 0; k < 10; ++k) {
+                const int e = i * 10 + j;
+                const int d = j * 10 + k;
+                // i / k == e / d
+                if ((i * d == k * e) && (e % 10) && (d % 10) && (i != j) && (j != k))
+                    cout << e << "/" << d << endl;
+            }
+        }
+    }
     return 0;
 }
