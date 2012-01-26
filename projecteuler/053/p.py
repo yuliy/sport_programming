03579320dfef612796b44cfa7979efc126951304
yuliy <aivyulik@gmail.com>
2012-01-26 21:02:22 +0400

    Problem #53 solution

--- projecteuler/053/p.py
+++ projecteuler/053/p.py
@@ -2,55 +2,27 @@
 
 import math
 
-#PrimeNumbers = {2: None, 3: None, 5: None, 7: None}
+Factorial = []
 
-def IsPrime(num):
-    #if (num, None) in PrimeNumbers:
-    #    return True
+def Init():
+    t = 1
+    Factorial.append(t)
+    for i in xrange(1, 101):
+        t *= i
+        Factorial.append(t)
 
-    if num <= 1:
-        return False
-    if num == 2:
-        return True
-    max = int(math.sqrt(num)) + 1
-    for i in xrange(2, max + 1):
-        if num % i == 0:
-            return False
-
-    #PrimeNumbers.update( [(num, None)] )
-    return True
-
-def IsSuper(number):
-    num = str(number)
-    numLen = len(num)
-    res = False
-
-    for i in xrange(0, numLen):
-        t = int(num[i:])
-        if not IsPrime(t):
-            return False
-        
-        if i != 0:
-            t = int(num[:-i])
-            if not IsPrime(t):
-                return False
-    return True
+def CalcC(n, r):
+    return Factorial[n] / (Factorial[r] * Factorial[n-r])
 
 def main():
+    Init()
     res = 0
-    cnt = 0
-
-    for num in xrange(8, 1000 * 1000 * 1000):
-        #if num % 100000 == 0:
-        #    print '\tcurrent number = %d; primesDicLen = %d' % (num, len(PrimeNumbers))
-
-        if IsSuper(num):
-            res += num
-            cnt += 1
-            print "cnt=", cnt, 'res=', res, 'num=', num
-            if cnt == 11:
-                print 'Result: ', res
-                return
+    for n in xrange(1, 101):
+        print 'n=', n, 'res=', res
+        for r in xrange(0, n+1):
+            if CalcC(n, r) > 1000000:
+                res += 1
+    print res
 
 if __name__ == '__main__':
     main()
