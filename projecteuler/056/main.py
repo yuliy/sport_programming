73943e771ba160fcfda8015c4485505d65940bdf
yuliy <aivyulik@gmail.com>
2012-01-29 14:05:50 +0400

    Problem #56 solution

--- projecteuler/056/main.py
+++ projecteuler/056/main.py
@@ -2,55 +2,22 @@
 
 import math
 
-#PrimeNumbers = {2: None, 3: None, 5: None, 7: None}
-
-def IsPrime(num):
-    #if (num, None) in PrimeNumbers:
-    #    return True
-
-    if num <= 1:
-        return False
-    if num == 2:
-        return True
-    max = int(math.sqrt(num)) + 1
-    for i in xrange(2, max + 1):
-        if num % i == 0:
-            return False
-
-    #PrimeNumbers.update( [(num, None)] )
-    return True
-
-def IsSuper(number):
-    num = str(number)
-    numLen = len(num)
-    res = False
-
-    for i in xrange(0, numLen):
-        t = int(num[i:])
-        if not IsPrime(t):
-            return False
-        
-        if i != 0:
-            t = int(num[:-i])
-            if not IsPrime(t):
-                return False
-    return True
+def calcSumOfDigits(num):
+    res = 0
+    while num:
+        res += (num % 10)
+        num /= 10
+    return res
 
 def main():
     res = 0
-    cnt = 0
-
-    for num in xrange(8, 1000 * 1000 * 1000):
-        #if num % 100000 == 0:
-        #    print '\tcurrent number = %d; primesDicLen = %d' % (num, len(PrimeNumbers))
-
-        if IsSuper(num):
-            res += num
-            cnt += 1
-            print "cnt=", cnt, 'res=', res, 'num=', num
-            if cnt == 11:
-                print 'Result: ', res
-                return
+    for a in xrange(0, 100):
+        print 'a = %d' % a
+        for b in xrange(0, 100):
+            curRes = calcSumOfDigits(a ** b)
+            if curRes > res:
+                res = curRes
+    print 'Result = %d' % res
 
 if __name__ == '__main__':
     main()
