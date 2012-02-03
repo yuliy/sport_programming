bc84993c1b1ba0dc570ed3a420f3328d56b3073e
yuliy <aivyulik@gmail.com>
2012-02-04 03:45:47 +0400

    Problem #47 solution

--- projecteuler/template.cpp
+++ projecteuler/template.cpp
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <iostream>
 #include <vector>
+#include <deque>
 #include <string>
 #include <map>
 #include <set>
 #include <list>
 #include <algorithm>
+#include <cmath>
 using namespace std;
 
 int main() {
