d3d788b6351c85e5545ef78139e15ffd11f170ac
yuliy <aivyulik@gmail.com>
2012-02-09 03:57:03 +0400

    @

--- timus/1029/main.cpp
+++ timus/1029/main.cpp
@@ -8,28 +8,19 @@
 #include <algorithm>
 using namespace std;
 
+typedef long long i64;
+//typedef int i64;
+
 int M, N;
-int a[101][501];
-int d[101][501];
+i64 a[101][501];
+i64 d[101][501];
 int p[101][501];
-//vector<int> tmpvec(501);
-//vector< vector<int> > a;
-//vector< vector<int> > d;
-//vector< vector<int> > p;
 
 static void Init() {
-    /*
-    for (int i = 0; i <= 100; ++i) {
-        a.push_back(tmpvec);
-        d.push_back(tmpvec);
-        p.push_back(tmpvec);
-    }
-    */
-
     scanf("%d %d", &M, &N);
     for (int i = 1; i <= M; ++i) {
         for (int j = 1; j <= N; ++j) {
-            scanf("%d", &(a[i][j]));
+            scanf("%lld", &(a[i][j]));
             d[i][j] = 2e9;
             p[i][j] = -1;
         }
@@ -43,20 +34,39 @@ static void Init() {
 int main() {
     Init();
 
+    if (M == 1) {
+        i64 min = 2e9;
+        int minIdx = -1;
+        for (int i = 1; i <= N; ++i) {
+            if (d[1][i] < min) {
+                min = d[1][i];
+                minIdx = i;
+            }
+        }
+        cout << minIdx << endl;
+        return 0;
+    }
+
+    if (N == 1) {
+        for (int i = 1; i <= M; ++i)
+            cout << "1 ";
+        return 0;
+    }
+
     for (int i = 2; i <= M; ++i) {
         // left to right
         for (int j = 1; j <= N; ++j) {
-            int &curd = d[i][j];
+            i64 &curd = d[i][j];
             int &curp = p[i][j];
 
-            const int t = a[i][j] + d[i-1][j];
+            const i64 t = a[i][j] + d[i-1][j];
             if (t < curd) {
                 curd = t;
                 curp = j;
             }
 
             if (j > 1) {
-                const int t = a[i][j] + d[i][j-1];
+                const i64 t = a[i][j] + d[i][j-1];
                 if (t < curd) {
                     curd = t;
                     curp = p[i][j-1];
@@ -66,17 +76,17 @@ int main() {
 
         // right to left
         for (int j = N; j >= 1; --j) {
-            int &curd = d[i][j];
+            i64 &curd = d[i][j];
             int &curp = p[i][j];
 
-            const int t = a[i][j] + d[i-1][j];
+            const i64 t = a[i][j] + d[i-1][j];
             if (t < curd) {
                 curd = t;
                 curp = j;
             }
 
             if (j < N) {
-                const int t = a[i][j] + d[i][j+1];
+                const i64 t = a[i][j] + d[i][j+1];
                 if (t < curd) {
                     curd = t;
                     curp = p[i][j+1];
@@ -94,7 +104,7 @@ int main() {
     */
 
     int minj = 0;
-    int minCost = 1e9;
+    i64 minCost = 1e9;
     for (int j = 1; j <= N; ++j) {
         if (d[M][j] < minCost) {
             minCost = d[M][j];
