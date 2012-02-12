f9ab81ac689dbd8d58490df231589456184d4a86
yuliy <aivyulik@gmail.com>
2012-02-12 14:58:50 +0400

    Problem #63 solution

--- projecteuler/063/main.py
+++ projecteuler/063/main.py
@@ -4,14 +4,45 @@ import math
 
 def main():
     res = 0
-    for i in xrange(1, 101):
-        print 'i=%s' % i
-        for p in xrange(1, 101):
+    """
+    for i in xrange(1, 11):
+        print 'i=%s\tres=%s' % (i, res)
+        for p in xrange(1, 1001):
             num = i ** p
-            print num
-            d = len(str(num))
+            d = math.log10(num)
+            d = int(math.floor(d)) + 1
+
             if d == p:
-                ++res
+                res += 1
+
+            print '----'
+            if d == p:
+                print '!!!'
+            print 'i=%s\tp=%s\tnum=%s\td=%s' % (i, p, num, d)
+    """
+
+    l = [math.log10(i) for i in xrange(1, 10)]
+
+    for lg in l:
+        print 'lg=%s' % (lg, )
+        for n in xrange(1, 10000):
+            t = n * lg
+            t = math.floor(t)
+            if t == (n - 1):
+                res += 1
+                print '\tn=%s' % (n, )
+            elif t > (n-1):
+                break
+
+    """
+    for n in xrange(1, 100):
+        if n % 1000000 == 0:
+            print 'n=%s\tres=%s' % (n, res)
+        t = (n-1) / n
+        if t in l:
+            res += 1
+            print 'n=%s' % (n,)
+    """
 
     print 'Result:', res
 
