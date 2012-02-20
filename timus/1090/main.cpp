dd5e2f41af035b9fc045db886139c031538b4782
yuliy <aivyulik@gmail.com>
2012-02-21 02:21:46 +0400

    @

--- timus/1090/main.cpp
+++ timus/1090/main.cpp
@@ -8,7 +8,56 @@
 #include <algorithm>
 using namespace std;
 
+int N, K;
+
+struct TCompare {
+    bool operator()(const pair<int, int> &l, const pair<int, int> &r) const {
+        return l.second > r.second;
+    }
+};
+
+static int Solve(vector< pair<int, int> > &v) {
+    std::sort(v.begin(), v.end(), TCompare());
+
+    int res = 0;
+    for (int i = 0; i < N; ++i) {
+        cout << "(" << v[i].first << "; " << i << ")\t";
+        if (v[i].first > i)
+            res += (v[i].first - i);
+    }
+
+    cout << res << "\t";
+    cout << endl;
+
+    return res;
+}
+
 int main() {
+    scanf("%d %d", &N, &K);
+
+    if (K == 1) {
+        cout << K << endl;
+        return 0;
+    }
+
+    int max = -1;
+    int maxRow = 1;
+    vector< pair<int, int> > v(N);
+    for (int i = 0; i < K; ++i) {
+        for (int j = 0; j < N; ++j) {
+            int tmp;
+            scanf("%d", &tmp);
+            v[j] = make_pair(j, tmp);
+        }
+
+        const int cur = Solve(v);
+        if (cur > max) {
+            max = cur;
+            maxRow = i + 1;
+        }
+    }
+
+    cout << maxRow << endl;
 
     return 0;
 }
