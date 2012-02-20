691aa8049c5a9e66d2e6ab7a020f66671d368074
Yuliy Ayvazyan <yuliy@yuliy-laptop.(none)>
2012-02-20 21:53:23 +0400

    @

--- timus/1003/main.cpp
+++ timus/1003/main.cpp
@@ -11,13 +11,19 @@ using namespace std;
 int main() {
     int N;
     int Q;
-    scanf("%d", &N);
-    scanf("%d", &Q);
-
     char buf[8];
-    for (int i = 0; i < Q; ++i) {
-        int beg, end;
-        scanf("%d %d %s", buf);
+
+    for (;;) {
+        scanf("%d", &N);
+        if (N == -1)
+            continue;
+
+        scanf("%d", &Q);
+
+        for (int i = 0; i < Q; ++i) {
+            int beg, end;
+            scanf("%d %d %s", buf);
+        }
     }
 
     return 0;
