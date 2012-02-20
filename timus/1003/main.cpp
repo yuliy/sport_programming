3368bb071c1288b5093828e07552945c18bb5d34
Yuliy Ayvazyan <yuliy@yuliy-laptop.(none)>
2012-02-20 21:43:02 +0400

    @

--- timus/1003/main.cpp
+++ timus/1003/main.cpp
@@ -9,6 +9,16 @@
 using namespace std;
 
 int main() {
+    int N;
+    int Q;
+    scanf("%d", &N);
+    scanf("%d", &Q);
+
+    char buf[8];
+    for (int i = 0; i < Q; ++i) {
+        int beg, end;
+        scanf("%d %d %s", buf);
+    }
 
     return 0;
 }
