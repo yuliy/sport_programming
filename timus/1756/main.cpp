da781d59d7a22d6c85ba567eccb0aff4f2b891de
Yuliy Ayvazyan <yuliy@yuliy-laptop.(none)>
2012-02-21 18:06:48 +0400

    1756 solution

--- timus/1756/main.cpp
+++ timus/1756/main.cpp
@@ -9,6 +9,17 @@
 using namespace std;
 
 int main() {
+    int m, d1, d2;
+    scanf("%d %d %d", &m, &d1, &d2);
+
+    const int a = (m * d1) / d2;
+    const int b = (m * d1) % d2;
+    for (int i = 0; i < d2; ++i) {
+        if (i < b)
+            printf("%d ", a + 1);
+        else
+            printf("%d ", a);
+    }
 
     return 0;
 }
