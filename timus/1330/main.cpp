f38e97986189a00334d9f80a33a7317d65efb459
Yuliy Ayvazyan <yuliy@yuliy-laptop.(none)>
2012-02-21 15:19:07 +0400

    1330 solution

--- timus/1330/main.cpp
+++ timus/1330/main.cpp
@@ -8,7 +8,29 @@
 #include <algorithm>
 using namespace std;
 
+const int MAX_N = 10000;
+int mass[MAX_N];
+
 int main() {
+    int N;
+    scanf("%d", &N);
+
+    for (int i = 0; i < N; ++i)
+        scanf("%d", mass + i);
+    for (int i = 1; i < N; ++i)
+        mass[i] += mass[i-1];
+
+    int Q;
+    scanf("%d", &Q);
+    for (int i = 0; i < Q; ++i) {
+        int a, b;
+        scanf("%d %d", &a, &b);
+
+        int res = mass[b-1];
+        if (a > 0)
+            res -= mass[a-2];
+        printf("%d\n", res);
+    }
 
     return 0;
 }
