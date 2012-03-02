94cb234380050ad68da242e75ef54a3c9ffd038f
Yuliy Ayvazyan <yuliy@yuliy-laptop.(none)>
2012-03-02 19:50:35 +0400

    1009 solution

--- timus/1009/main.cpp
+++ timus/1009/main.cpp
@@ -1,14 +1,38 @@
 #include <stdio.h>
-#include <iostream>
-#include <vector>
-#include <string>
-#include <map>
-#include <set>
-#include <list>
-#include <algorithm>
-using namespace std;
+//#include <vector>
+//#include <stdlib.h>
+//#include <math.h>
+//using namespace std;
 
-int main() {
+int main()
+{
+   // Рекуррентная формула получилась вот такая:
+   // Mn+1 = (K - 1) * (Mn + Mn-1);
+   // Начальное условие: M2 = K * (K - 1)
+   //                    M3 = (K - 1) * (K - 1) * (K + 1)
 
-    return 0;
+   int N, K;
+   scanf( "%d%d", &N, &K );
+
+   long long M2 = K * (K - 1);
+   long long M3 = (K - 1) * (K - 1) * (K + 1);
+
+   int res;
+   if( N == 2 )
+      res = M2;
+   else if( N == 3 )
+      res = M3;
+   else
+   {
+      for( int i = 3; i < N; ++i )
+      {
+         res = (K - 1) * (M3 + M2);
+         M2 = M3;
+         M3 = res;
+      }
+   }
+
+   printf( "%d\n", res );
+
+   return 0;
 }
