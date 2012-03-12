69798f5ad5bf0b228ec49ef432a7f766ff2dbf84
yuliy <aivyulik@gmail.com>
2012-03-12 14:58:47 +0400

    Fixed operator*

--- algorithms/matrix/main.cpp
+++ algorithms/matrix/main.cpp
@@ -10,14 +10,14 @@ using namespace std;
 
 typedef unsigned int ui32;
 
-template<typename T, int COLUMNS, int ROWS>
+template<typename T, int ROWS, int COLUMNS>
 class TMatrix {
 private:
-    T Elems[COLUMNS][ROWS];
+    T Elems[ROWS][COLUMNS];
 
     void Zero() {
-        for (int i = 0; i < COLUMNS; ++i)
-            for (int j = 0; j < ROWS; ++j)
+        for (int i = 0; i < ROWS; ++i)
+            for (int j = 0; j < COLUMNS; ++j)
                 Elems[i][j] = T();
     }
 
@@ -27,68 +27,68 @@ public:
     }
 
     //template<typename U>
-    //TMatrix(const TMatrix<U, COLUMNS, ROWS> &other) {
+    //TMatrix(const TMatrix<U, ROWS, COLUMNS> &other) {
     TMatrix(const TMatrix &other) {
         *this = other;
     }
 
     //template<typename U>
-    //TMatrix<T, COLUMNS, ROWS>& operator=(const TMatrix<U, COLUMNS, ROWS> &other) {
+    //TMatrix<T, ROWS, COLUMNS>& operator=(const TMatrix<U, ROWS, COLUMNS> &other) {
     TMatrix& operator=(const TMatrix &other) {
-        for (int i = 0; i < COLUMNS; ++i)
-            for (int j = 0; j < ROWS; ++j)
+        for (int i = 0; i < ROWS; ++i)
+            for (int j = 0; j < COLUMNS; ++j)
                 Elems[i][j] = other.Elems[i][j];
                 //Elems[i][j] = other.Get(i, j);
         return *this;
     }
 
-    void Set(int i, int j, const T& elem) {
-        Elems[i][j] = elem;
+    const T& operator()(int i, int j) const {
+        return Elems[i][j];
     }
 
-    const T& Get(int i, int j) const {
+    T& operator()(int i, int j) {
         return Elems[i][j];
     }
 
     TMatrix operator+(const TMatrix &other) {
         TMatrix res;
-        for (int i = 0; i < COLUMNS; ++i)
-            for (int j = 0; j < ROWS; ++j)
+        for (int i = 0; i < ROWS; ++i)
+            for (int j = 0; j < COLUMNS; ++j)
                 res.Elems[i][j] = this->Elems[i][j] + other.Elems[i][j];
         return res;
     }
 
     TMatrix operator-(const TMatrix &other) {
         TMatrix res;
-        for (int i = 0; i < COLUMNS; ++i)
-            for (int j = 0; j < ROWS; ++j)
+        for (int i = 0; i < ROWS; ++i)
+            for (int j = 0; j < COLUMNS; ++j)
                 res.Elems[i][j] = this->Elems[i][j] - other.Elems[i][j];
         return res;
     }
 
-    template<int OTHER_ROWS>
-    TMatrix<T, COLUMNS, OTHER_ROWS> operator*(const TMatrix<T, ROWS, OTHER_ROWS> &other) {
-        TMatrix<T, COLUMNS, OTHER_ROWS> res;
-        for (int i = 0; i < COLUMNS; ++i) {
-            for (int j = 0; j < ROWS; ++j) {
-                T tmp();
-                for (int k = 0; k < OTHER_ROWS; ++k) {
-                    tmp += this->Elems[i][j] * other.Get(j, k);
+    template<int OTHER_COLUMNS>
+    TMatrix<T, ROWS, OTHER_COLUMNS> operator*(const TMatrix<T, COLUMNS, OTHER_COLUMNS> &other) {
+        TMatrix<T, ROWS, OTHER_COLUMNS> res;
+        for (int i = 0; i < ROWS; ++i) {
+            for (int j = 0; j < OTHER_COLUMNS; ++j) {
+                T tmp = T();
+                for (int k = 0; k < COLUMNS; ++k) {
+                    tmp += this->Elems[i][k] * other(k, j);
                 }
-                res.Set(i, j, tmp);
+                res(i, j) = tmp;
             }
         }
         return res;
     }
 
-    template<typename pT, int pCOLUMNS, int pROWS>
-    friend ostream &operator<<(ostream &ous, const TMatrix<pT, pCOLUMNS, pROWS> &mx);
+    template<typename pT, int pROWS, int pCOLUMNS>
+    friend ostream &operator<<(ostream &ous, const TMatrix<pT, pROWS, pCOLUMNS> &mx);
 };
 
-template<typename T, int COLUMNS, int ROWS>
-ostream &operator<<(ostream &ous, const TMatrix<T, COLUMNS, ROWS> &mx) {
-    for (int i = 0; i < COLUMNS; ++i) {
-        for (int j = 0; j < ROWS; ++j)
+template<typename T, int ROWS, int COLUMNS>
+ostream &operator<<(ostream &ous, const TMatrix<T, ROWS, COLUMNS> &mx) {
+    for (int i = 0; i < ROWS; ++i) {
+        for (int j = 0; j < COLUMNS; ++j)
             ous << mx.Elems[i][j] << "\t";
         ous << "\n";
     }
@@ -97,10 +97,24 @@ ostream &operator<<(ostream &ous, const TMatrix<T, COLUMNS, ROWS> &mx) {
 
 int main( int argc, char** argv ) {
     try {
-        TMatrix<int, 3, 3> m1;
-        TMatrix<int, 3, 3> m2(m1);
+        TMatrix<int, 2, 3> m1;
+        TMatrix<int, 3, 4> m2;
+
+        // m1
+        for (int i = 0; i < 2; ++i)
+            for (int j = 0; j < 3; ++j)
+                m1(i, j) = i + j;
+
+        // m2
+        for (int i = 0; i < 3; ++i)
+            for (int j = 0; j < 4; ++j)
+                m2(i, j) = i - j;
+
         cout << m1 << endl
-             << m2 << endl;
+            << m2 << endl;
+
+        TMatrix<int, 2, 4> m3 = m1 * m2;
+        cout << m3 << endl;
     } catch (const exception &xcp) {
         cout << "An std::exception occured in main routine: " << xcp.what() << endl;
     } catch (...) {
