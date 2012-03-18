78721dde0044fad4c1476ee9f6b5f5eb78fcce2a
yuliy <aivyulik@gmail.com>
2012-03-18 23:21:58 +0400

    @

--- timus/1640/main.cpp
+++ timus/1640/main.cpp
@@ -6,9 +6,22 @@
 #include <set>
 #include <list>
 #include <algorithm>
+#include <math.h>
 using namespace std;
 
 int main() {
+    int N;
+    scanf("%d", &N);
 
+    double maxr = 0.0;
+    for (int i = 0; i < N; ++i) {
+        int x, y;
+        scanf("%d %d", &x, &y);
+        const double r = sqrt(1.0 * x * x + 1.0 * y * y);
+        if (r > maxr)
+            maxr = r;
+    }
+
+    printf("0 0 %.10f\n", maxr);
     return 0;
 }
