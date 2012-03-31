2e4f16c6f4c74e89a212785fddfac9511b810640
yuliy <aivyulik@gmail.com>
2012-04-01 02:49:29 +0400

    @

--- algorithms/convex_hull/main.cpp
+++ algorithms/convex_hull/main.cpp
@@ -32,7 +32,7 @@ struct TPoint2D {
 };
 
 typedef TPoint2D<float> TPoint;
-typedef vector<TPoint> TPoints;
+typedef vector< TPoint > TPoints;
 
 template<typename T, typename U>
 struct TFirstComparator {
@@ -47,8 +47,8 @@ private:
 private:
     int FindStartPoint(const TPoints &v) const {
         int res = 0;
-        double minx = v[0].X;
-        double miny = v[0].Y;
+        float minx = v[0].X;
+        float miny = v[0].Y;
         const int cnt = Points.size();
         for (int i = 1; i < cnt; ++i) {
             if (v[i].Y < miny || (v[i].Y == miny && v[i].X < minx)) {
@@ -61,32 +61,52 @@ private:
     }
 
     void CalcPointsSortedByPolarAngle(TPoints &res, int startIdx) const {
+        const TPoint &sPoint = Points[startIdx];
+
         vector< pair<double, TPoint> > v;
-        const int cnt = Point.size();
+        const int cnt = Points.size();
         for (int i = 0; i < cnt; ++i) {
             if (i == startIdx)
                 continue;
 
-            const double dx = Points[i].X - Points[startIdx].X;
-            const double dy = Points[j].Y - Points[startIdx].Y;
-            const double tg = (dx == 0.0) ? 0.0 : (dx / dy);
-            v.push_back( make_pair(tg, i) );
+            const double dx = Points[i].X - sPoint.X;
+            const double dy = Points[i].Y - sPoint.Y;
+            const double atg = (dy == 0.0) ? ((dx > 0.0) ? 1e9 : -1e9) : (dx / dy);
+            v.push_back( make_pair(atg, Points[i]) );
         }
 
         sort(v.begin(), v.end(), TFirstComparator<double, TPoint>());
 
-        TPoints res;
+        double prev_atg = 0.0;
         for (vector< pair<double, TPoint> >::const_iterator iter = v.begin(); iter != v.end(); ++iter) {
             const TPoint &p = iter->second;
             if (res.empty()) {
                 res.push_back(p);
+                prev_atg = iter->first;
                 continue;
             }
 
-            const TPoint &prev = res.back();
-            //
+            if (iter->first != prev_atg) {
+                res.push_back(p);
+            } else {
+                const TPoint &prev = res.back();
+                const double dd = (p.X - sPoint.X) * (p.X - sPoint.X) + (p.Y - sPoint.Y) * (p.Y - sPoint.Y);
+                const double prev_dd = (prev.X - sPoint.X) * (prev.X - sPoint.X) + (prev.Y - sPoint.Y) * (prev.Y - sPoint.Y);
+                if (dd > prev_dd)
+                    res.back() = p;
+            }
+
+            prev_atg = iter->first;
         }
     }
+
+    bool NonLeftTurn(const TPoint &p0, const TPoint &p1, const TPoint &p2) const {
+        const TPoint a(p2.X - p0.X, p2.Y - p0.Y);
+        const TPoint b(p1.X - p0.X, p2.Y - p0.Y);
+        const float p = a.X * b.Y - b.X * a.Y;
+        return p >= 0.0;
+    }
+
 public:
     TPolygon() {
     }
@@ -107,21 +127,34 @@ public:
         return Points[idx];
     }
 
-    void CalcConvexHull(TPoints &cv) const {
-        const int idx = FindStartPoint(Points);
+    void CalcConvexHull_Graham(TPolygon &ch) const {
+        const int startIdx = FindStartPoint(Points);
+        const TPoint &sPoint = Points[startIdx];
         
         TPoints sortedPoints;
-        CalcPointsSortedByPolarAngle(sortedPoints, idx);
-    /*
+        CalcPointsSortedByPolarAngle(sortedPoints, startIdx);
 
-        std::sort(v.begin(), v.end(), TFirstComparator<double, int>());
+        vector<TPoint> st;
+        st.push_back(sPoint);
+        st.push_back(sortedPoints[0]);
+        st.push_back(sortedPoints[1]);
 
-        vector<int> st;
-        st.push_back(idx);
-        st.push_back(v[0].second);
-        st.push_back(v[1].second);
-        */
+        const int m = sortedPoints.size();
+        for (int i = 2; i < m; ++i) {
+            const TPoint &pi = sortedPoints[i];
+            while ( NonLeftTurn(st[st.size()-2], st[st.size()-1], pi) )
+                st.pop_back();
+            st.push_back(pi);
+        }
+
+        ch = TPolygon(st);
     }
+
+    /*
+    void CalcConvexHull_Jarvis(TPoints &cv) const {
+        
+    }
+    */
 
     //bool HasInside(const TPoint &p) const {
     //}
@@ -148,6 +181,10 @@ int main( int argc, char** argv ) {
     try {
         const TPolygon poly = CreateRandomPolygon(10);
         cout << poly << endl;
+        TPolygon ch;
+        poly.CalcConvexHull_Graham(ch);
+        cout << "-----------------------------------" << endl
+            << ch << endl;
     } catch (const exception &xcp) {
         cout << "An std::exception occured in main routine: " << xcp.what() << endl;
     } catch (...) {
