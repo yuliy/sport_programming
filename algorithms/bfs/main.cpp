88b49ee8eca08dc0d20a56a6b0a3e466c37ab11b
yuliy <aivyulik@gmail.com>
2012-04-01 17:17:26 +0400

    bfs algorithm draft

---
+++ algorithms/bfs/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <exception>
+#include <vector>
+#include <algorithm>
+#include <functional>
+#include <iterator>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+
+typedef unsigned int ui32;
+typedef long long i64;
+
+i64 GetTickCount() {
+    return clock() * 1000 / CLOCKS_PER_SEC;
+}
+
+int main( int argc, char** argv ) {
+    try {
+    } catch (const exception &xcp) {
+        cout << "An std::exception occured in main routine: " << xcp.what() << endl;
+    } catch (...) {
+        cout << "An unknown exception occured in main routine!" << endl;
+    }
+
+    return 0;
+}
