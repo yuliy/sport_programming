047fb467f93718acce9829dc02126c437c5402e1
yuliy <aivyulik@gmail.com>
2012-04-29 00:22:16 +0400

    @

---
+++ algorithms/red_black_tree/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <exception>
+#include <vector>
+#include <algorithm>
+#include <functional>
+#include <iterator>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+
+typedef unsigned int ui32;
+typedef long long i64;
+
+i64 GetTickCount() {
+    return clock() * 1000 / CLOCKS_PER_SEC;
+}
+
+int main( int argc, char** argv ) {
+    try {
+        //srand( time(NULL) );
+        //Test();
+    } catch (const exception &xcp) {
+        cout << "An std::exception occured in main routine: " << xcp.what() << endl;
+    } catch (...) {
+        cout << "An unknown exception occured in main routine!" << endl;
+    }
+
+    return 0;
+}
