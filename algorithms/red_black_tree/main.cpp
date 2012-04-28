44216314868abd03e91d7c65fd15525d7c8cd45a
yuliy <aivyulik@gmail.com>
2012-04-29 01:34:58 +0400

    @

--- algorithms/red_black_tree/main.cpp
+++ algorithms/red_black_tree/main.cpp
@@ -15,6 +15,51 @@ i64 GetTickCount() {
     return clock() * 1000 / CLOCKS_PER_SEC;
 }
 
+enum EColour {
+    C_BLACK,
+    C_RED,
+};
+
+template<typename T>
+struct TNode {
+    EColour Colour;
+    TNode *Parent, *Left, *Right;
+    T Value;
+
+    TNode()
+        : Colour(C_BLACK)
+        , Parent(0)
+        , Left(0)
+        , Right(0)
+        , Value(T()) {
+    }
+
+    TNode(EColour colour, TNode *parent, TNode *left, TNode *right, const T &value)
+        : Colour(colour)
+        , Parent(parent)
+        , Left(left)
+        , Right(right)
+        , Value(value) {
+    }
+};
+
+template<typename T, typename TComparator = std::less<T> >
+class TRBTree {
+private:
+    TNode *Root;
+    TComparator Cmp;
+public:
+    TRBTree()
+        : Cmp(TComparator())
+        , Root(0) {
+    }
+
+    TRBTree(TComparator cmp)
+        : Cmp(cmp)
+        , Root(0) {
+    }
+};
+
 int main( int argc, char** argv ) {
     try {
         //srand( time(NULL) );
