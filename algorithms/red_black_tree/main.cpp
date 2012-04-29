e34684a474c31c387493d0f72a12d9ffc84c6c03
yuliy <aivyulik@gmail.com>
2012-04-30 01:20:53 +0400

    @

--- algorithms/red_black_tree/main.cpp
+++ algorithms/red_black_tree/main.cpp
@@ -6,6 +6,7 @@
 #include <iterator>
 #include <iomanip>
 #include <cmath>
+#include "rbtree.h"
 using namespace std;
 
 typedef unsigned int ui32;
@@ -15,111 +16,22 @@ i64 GetTickCount() {
     return clock() * 1000 / CLOCKS_PER_SEC;
 }
 
-enum EColour {
-    C_BLACK,
-    C_RED,
-};
+static void Test() {
+    TRBTree<int> t;
 
-template<typename T>
-struct TNode {
-    EColour Colour;
-    TNode *Parent, *Left, *Right;
-    T Key;
-
-    TNode()
-        : Colour(C_BLACK)
-        , Parent(0)
-        , Left(0)
-        , Right(0)
-        , Key(T()) {
+    const int cnt = 10;
+    for (int i = 0; i < cnt; ++i) {
+        cout << "inserting: " << i << endl;
+        TNode<int> *n = new TNode<int>();
+        n->Key = i;
+        t.Insert(n);
     }
-
-    TNode(EColour colour, TNode *parent, TNode *left, TNode *right, const T &key)
-        : Colour(colour)
-        , Parent(parent)
-        , Left(left)
-        , Right(right)
-        , Key(key) {
-    }
-};
-
-template<typename T, typename TComparator = std::less<T> >
-class TRBTree {
-private:
-    TNode *Root;
-    //TNode *Sentinel;
-    TComparator Cmp;
-private:
-    void LeftRotate(TNode *x) {
-        TNode *y = x->Right;        // set y
-        x->Right = y->Left;         // turn y's left subtree into x's right subtree
-        if (y->Left != 0)
-            y->Left->Parent = x;
-        y->Parent = x->Parent;
-        if (x->Parent == 0)
-            Root = y;
-        else if (x == x->Parent->Left)
-            x->Parent->Left = y;
-        else
-            x->Parent->Right = y;
-        y->Left = x;
-        x->Parent = y;
-    }
-
-    void RightRotate(TNode *y) {
-        TNode *x = y->Left;
-        y->Left = x->Right;
-        if (x->Right != 0)
-            x->Right->Parent = y;
-        x->Parent = y->Parent;
-        if (y->Parent == 0)
-            Root = x;
-        else if (y == y->Parent->Left)
-            y->Parent->Left = x;
-        else
-            y->Parent->Right = x;
-        x->Right = y;
-        y->Parent = x;
-    }
-
-    void Insert(TNode *z) {
-        TNode *y = 0;
-        TNode *x = Root;
-        while (x != 0) {
-            y = x;
-            x = Cmp(z->Key, x->Key) ? x->Left : x->Right;
-        }
-
-        z->Parent = y;
-        if (y == 0)
-            Root = z;
-        else if (Cmp(z->Key, y->Key))
-            y->Left = z;
-        else
-            y->Right = z;
-        z->Left = 0;
-        z->Right = 0;
-        z->Colour = C_RED;
-        InsertFixup(z);
-    }
-public:
-    TRBTree()
-        : Cmp(TComparator())
-        , Root(0) {
-        //, Sentinel (new TNode()) {
-     }
-
-    TRBTree(TComparator cmp)
-        : Cmp(cmp)
-        , Root(0) {
-        //, Sentinel(new TNode()) {
-    }
-};
+}
 
 int main( int argc, char** argv ) {
     try {
         //srand( time(NULL) );
-        //Test();
+        Test();
     } catch (const exception &xcp) {
         cout << "An std::exception occured in main routine: " << xcp.what() << endl;
     } catch (...) {
