5a17be89af54be6e2c1804faa0d9c38b07f501d2
yuliy <aivyulik@gmail.com>
2012-06-12 22:23:16 +0400

    @

--- timus/1069/main.cpp
+++ timus/1069/main.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
-#include <priority_queue>
+#include <queue>
 #include <map>
 #include <set>
 #include <list>
@@ -10,28 +10,57 @@
 using namespace std;
 
 typedef vector<int> TVertices;
-typedef map<int, int> TVertex2Count;
-typedef priority_queue<int, greater<int> > TQueue;
+//typedef map<int, int> TVertex2Count;
+//typedef priority_queue<int> TQueue;
+typedef priority_queue<int, vector<int>, greater<int> > TQueue;
+
+typedef set<int> TAdjList;
+typedef vector< TAdjList > TAdjLists;
 
 int main() {
     TVertices vertices;
-    TVertex2Count v2c;
+    //TVertex2Count v2c;
+    int v2c[8000] = {0};
     int tmp;
+    int N = 0;
     while (scanf("%d", &tmp) != EOF) {
         vertices.push_back(tmp);
         ++v2c[tmp];
+        if (tmp > N)
+            N = tmp;
     }
 
+    //greater<int> cmp;
     TQueue q;
-    for (TVertex2Count::const_iterator iter = v2c.begin(), end = v2c.end(); iter != end; ++iter)
-        if (iter->second == 0)
-            q.push(iter->first);
+    //for (TVertex2Count::const_iterator iter = v2c.begin(), end = v2c.end(); iter != end; ++iter)
+    for (int i = 1; i <= N; ++i)
+        if (v2c[i] == 0)
+            q.push(i);
+
+    TAdjLists adjLists(N);
 
     for (TVertices::const_iterator iter = vertices.begin(), end = vertices.end(); iter != end; ++iter) {
         const int v = *iter;
+        if (q.empty())
+            continue;
+
         const int minv = q.top();
 
-        //
+        adjLists[minv-1].insert(v);
+        adjLists[v-1].insert(minv);
+
+        if (v2c[v] == 0)
+            q.push(v);
+        else
+            --v2c[v];
+    }
+
+    for (int i = 0; i < N; ++i) {
+        printf("%d:", i + 1);
+        const TAdjList &lst = adjLists[i];
+        for (TAdjList::const_iterator iter = lst.begin(), end = lst.end(); iter != end; ++iter)
+            printf(" %d", *iter);
+        printf("\n");
     }
 
     return 0;
