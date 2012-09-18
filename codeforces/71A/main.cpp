ae3fd1da7a4e967738494b1623c913acfadde9cf
Yuliy Ayvazyan <aivyulik@gmail.com>
2012-09-18 13:54:09 +0400

    codeforces 71A solution

--- codeforces/71A/main.cpp
+++ codeforces/71A/main.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <iostream>
 #include <vector>
+#include <cstring>
 #include <string>
 #include <map>
 #include <set>
@@ -9,6 +10,18 @@
 using namespace std;
 
 int main() {
+    int N;
+    scanf("%d", &N);
+
+    char buf[128];
+    for (int i = 0; i < N; ++i) {
+        scanf("%s", buf);
+        const int len = strlen(buf);
+        if (len <= 10)
+            printf("%s\n", buf);
+        else
+            printf("%c%d%c\n", buf[0], len-2, buf[len-1]);
+    }
 
     return 0;
 }
