b7c36d8ce5cafb7a7a6aeef14aaf7377173e079c
Yuliy Ayvazyan <aivyulik@gmail.com>
2012-09-18 17:48:02 +0400

    codeforces 82A solution

--- codeforces/082A/main.cpp
+++ codeforces/082A/main.cpp
@@ -9,7 +9,32 @@
 #include <algorithm>
 using namespace std;
 
-int main() {
+const char* names[] = {
+    "Howard",
+    "Sheldon",
+    "Leonard",
+    "Penny",
+    "Rajesh",
+    "Howard",
+};
 
+int main() {
+    int N;
+    scanf("%d", &N);
+
+    int tmp = 5;
+    int dup = 1;
+    while (N > tmp) {
+        N -= tmp;
+        tmp *= 2;
+        dup *= 2;
+        //cout << "N=" << N << "\ttmp=" << tmp << "\tdup=" << dup << endl;
+    }
+
+    int num = N / dup;
+    if (N % dup)
+        ++num;
+    //cout << "num=" << num << endl;
+    printf("%s\n", names[num]);
     return 0;
 }
