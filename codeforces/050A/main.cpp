d7e72c5e2b53aab0023ab6036c82912413494801
Yuliy Ayvazyan <aivyulik@gmail.com>
2012-09-19 01:54:18 +0400

    codeforces 50A solution

---
+++ codeforces/050A/main.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <cstring>
+#include <string>
+#include <map>
+#include <set>
+#include <list>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int m, n;
+    scanf("%d %d", &m, &n);
+    printf("%d\n", (m * n) / 2);
+    return 0;
+}
