a93d992cc47e5c62e0cce57509c7c471aedb22ac
Yuliy Ayvazyan <yuliy@yuliy.me>
2012-09-27 16:22:39 +0400

    @

--- codeforces/002B/main.cpp
+++ codeforces/002B/main.cpp
@@ -10,6 +10,81 @@
 #include <algorithm>
 using namespace std;
 
+int N;
+bool HasZero = false;
+int ZeroI = -1;
+int ZeroJ = -1;
+
+const int SIZE = 1001;
+int D2[SIZE][SIZE];
+int D5[SIZE][SIZE];
+
+int C2[SIZE][SIZE];
+int C5[SIZE][SIZE];
+
+int P2[SIZE][SIZE];
+int P5[SIZE][SIZE];
+
+const int DOWN = 1;
+const int RIGHT = 2;
+
+static void Init() {
+    scanf("%d", &N);
+    for (int i = 0; i <= N; ++i) {
+        for (int j = 0; j <=N; ++j) {
+            D2[i][j] = D5[i][j] = C2[i][j] = C5[i][j] = P2[i][j] = P5[i][j] = 0;
+        }
+    }
+
+    for (int i = 1; i <= N; ++i) {
+        for (int j = 1; j <= N; ++j) {
+            int num;
+            scanf("%d", &num);
+
+            if (num == 0) {
+                HasZero = true;
+                ZeroI = i;
+                ZeroJ = j;
+            }
+
+            for (; num && (num % 2 == 0); ++D2[i][j])
+                num /= 2;
+            for (; num && (num % 5 == 0); ++D5[i][j])
+                num /= 5;
+        }
+    }
+}
+
+static void CalcCountsAndPaths(int **C, int **P) {
+    for (int i = 1; i <= N; ++i) {
+        for (int j = 1; j <= N; ++j) {
+            if (i == 1) {
+                C[i][j] += C[i][j-1];
+                P[i][j] = RIGHT;
+            } else if (j == 1) {
+                C[i][j] += C[i-1][j];
+                P[i][j] = DOWN;
+            } else if (C[i-1][j] < C[i][j-1]) {
+                C[i][j] += C[i-1][j];
+                P[i][j] = DOWN;
+            } else {
+                C[i][j] += C[i][j-1];
+                P[i][j] = RIGHT;
+            }
+        }
+    }
+}
+
+int main() {
+    Init();
+
+    CalcCountsAndPaths(C2, P2);
+    CalcCountsAndPaths(C5, P5);
+
+    return 0;
+}
+
+/*
 int A[1001][1001];
 int D[1001][1001];
 int F[1001][1001];
@@ -76,3 +151,4 @@ int main() {
     printf("\n");
     return 0;
 }
+*/
