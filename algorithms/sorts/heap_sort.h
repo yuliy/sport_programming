6f86461e8bfa1e495197dc6e8dd450413605514b
Yuliy Ayvazyan <aivyulik@gmail.com>
2012-10-12 03:22:54 +0400

    @

--- algorithms/sorts/heap_sort.h
+++ algorithms/sorts/heap_sort.h
@@ -1,9 +1,25 @@
 #pragma once
 
+#include <vector>
+
 namespace ystd {
 
-    template<typename T>
+    template<typename T, typename TCont = std::vector<T> >
     class MaxHeap {
+    private:
+        TCont Cont;
+        typedef typename TCont::iterator TIter;
+        typedef typename TCont::const_iterator TCIter;
+    private:
+        //          0
+        //      1       2
+        //    3   4   5   6
+        //
+        TIter Parent(TIter beg, TIter iter) {
+            const int idx = iter - beg;
+            //const int parentIdx = (idx >> 1) + 
+        }
+    public:
         //
     };
 
