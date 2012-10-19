d1e3038b30700797e1f9df41e8d1d3c52a4c5286
Yuliy Ayvazyan <yuliy@yuliy.me>
2012-10-19 22:02:42 +0400

    codeforces 4C solution

---
+++ codeforces/004C/main.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <cstring>
+#include <string>
+#include <map>
+#include <set>
+#include <list>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int N;
+    scanf("%d", &N);
+    typedef map<string, int> TMap;
+    TMap name2count;
+    for (int i = 0; i < N; ++i) {
+        char buf[64];
+        scanf("%s", buf);
+
+        TMap::const_iterator iter = name2count.find(buf);
+        if (iter == name2count.end()) {
+            printf("OK\n");
+        } else {
+            printf("%s%d\n", buf, iter->second);
+        }
+        ++name2count[buf];
+    }
+    return 0;
+}
