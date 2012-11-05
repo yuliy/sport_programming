1ce1fbfa3d9993c27ec77a2a66d6bf4e5c8ebd2c
Yuliy Ayvazyan <aivyulik@gmail.com>
2012-11-05 12:32:56 +0400

    huffman_compression draft

---
+++ algorithms/huffman_compression/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <exception>
+#include <vector>
+#include <deque>
+#include <set>
+#include <algorithm>
+#include <functional>
+#include <iterator>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+using namespace ystd;
+
+typedef unsigned int ui32;
+typedef long long i64;
+
+static i64 GetTickCount() {
+    return clock() * 1000 / CLOCKS_PER_SEC;
+}
+
+static void Test() {
+}
+
+int main( int argc, char** argv ) {
+    try {
+    } catch (const exception &xcp) {
+        cout << "An std::exception occured in main routine: " << xcp.what() << endl;
+    } catch (...) {
+        cout << "An unknown exception occured in main routine!" << endl;
+    }
+
+    return 0;
+}
