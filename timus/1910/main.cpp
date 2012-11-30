d20fefe00af7121d0e8664e06e9bc576bf8cd409
Yuliy Ayvazyan <yuliy@yuliy.me>
2012-11-30 23:04:24 +0400

    @

--- timus/1910/main.cpp
+++ timus/1910/main.cpp
@@ -9,6 +9,22 @@
 using namespace std;
 
 int main() {
+    int n;
+    scanf("%d", &n);
+    vector<int> v(n);
+    for (int i = 0; i < n; ++i)
+        scanf("%d", &v[i]);
 
+    int idx = -1;
+    int sum = -1;
+    for (int i = 1; i < (n-1); ++i) {
+        const int s = v[i-1] + v[i] + v[i+1];
+        if (s > sum) {
+            sum = s;
+            idx = i;
+        }
+    }
+
+    printf("%d %d\n", sum, idx+1);
     return 0;
 }
