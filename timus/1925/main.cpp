4222cc24d0db6a11f199974e4d60d0de053b31f1
Yuliy Ayvazyan <yuliy@yuliy.me>
2012-11-30 23:23:40 +0400

    @

--- timus/1925/main.cpp
+++ timus/1925/main.cpp
@@ -9,6 +9,22 @@
 using namespace std;
 
 int main() {
+    int n, k;
+    scanf("%d %d", &n, &k);
 
+    int expected_sum = k - 2;
+    int cur_sum = 0;
+    for (int i = 0; i < n; ++i) {
+        int b, g;
+        scanf("%d %d", &b, &g);
+        expected_sum += (b-2);
+        cur_sum += g;
+    }
+
+    const int res = expected_sum - cur_sum;
+    if (res < 0)
+        printf("Big Bang!");
+    else
+        printf("%d\n", res);
     return 0;
 }
