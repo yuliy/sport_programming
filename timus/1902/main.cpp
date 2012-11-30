ad47b030af74bcb4813d0d138fe38f6cee0e08ed
Yuliy Ayvazyan <yuliy@yuliy.me>
2012-11-30 23:43:34 +0400

    @

--- timus/1902/main.cpp
+++ timus/1902/main.cpp
@@ -9,6 +9,20 @@
 using namespace std;
 
 int main() {
+    int n, t, s;
+    scanf("%d %d %d", &n, &t, &s);
 
+    for (int i = 0; i < n; ++i) {
+        int si;
+        scanf("%d", &si);
+        const double diff = abs(s - si);
+        if (s < si) {
+            const double res = si + (t - diff)/2.0;
+            printf("%.6f\n", res);
+        } else {
+            const double res = s + (t - diff)/2.0;
+            printf("%.6f\n", res);
+        }
+    }
     return 0;
 }
