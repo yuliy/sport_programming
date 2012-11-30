5506a3970411e94c74f6f7ea2f60f5c0be839b3f
Yuliy Ayvazyan <yuliy@yuliy.me>
2012-11-30 23:13:01 +0400

    @

--- timus/1924/main.cpp
+++ timus/1924/main.cpp
@@ -9,6 +9,12 @@
 using namespace std;
 
 int main() {
-
+    int n;
+    scanf("%d", &n);
+    const int cnt = (n >> 1) + (n % 2);
+    if (cnt % 2)
+        printf("grimy");
+    else
+        printf("black");
     return 0;
 }
