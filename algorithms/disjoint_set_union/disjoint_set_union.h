7f6e15877607115aec8dfca6d7d2ab419af5698c
Yuliy Ayvazyan <aivyulik@gmail.com>
2012-12-16 15:38:46 +0400

    bug-fix!

--- algorithms/disjoint_set_union/disjoint_set_union.h
+++ algorithms/disjoint_set_union/disjoint_set_union.h
@@ -2,6 +2,7 @@
 
 #include <list>
 #include <vector>
+#include <algorithm>
 #include <boost/shared_ptr.hpp>
 
 namespace ystd {
@@ -47,13 +48,15 @@ public:
     TNode *MakeSet(const T &val) {
         TPNode p(new TNode(val));
         p->Parent = p.get();
+        p->Rank = 0;
+        p->Value = val;
         PNodes.push_back(p);
         return p.get();
     }
 
     bool Union(TNode *x, TNode *y) {
         if (!x || !y)
-            return false;
+            throw "Achtung!";
 
         TNode *sx = FindSet(x);
         TNode *sy = FindSet(y);
@@ -61,9 +64,9 @@ public:
             throw "Achtung!";
 
         if (sx->Rank > sy->Rank) {
-            y->Parent = x;
+            sy->Parent = sx;
         } else {
-            x->Parent = y;
+            sx->Parent = sy;
             if (sx->Rank == sy->Rank)
                 sy->Rank += 1;
         }
@@ -82,6 +85,16 @@ public:
         if (x != x->Parent)
             x->Parent = FindSet(x->Parent);
         return x->Parent;
+        /*
+        for (TNode *node = x; node != node->Parent; node = node->Parent)
+            x->Parent = node;
+
+        for (TNode *node = x; node != node->Parent; node = node->Parent) {
+            node->Parent = x->Parent;
+        }
+
+        return x->Parent;
+        */
     }
 
     TNode *FindSet(TPNode x) {
@@ -89,4 +102,48 @@ public:
     }
 };
 
+/**
+  * TDisjointSetUnion2 is a disjoint set union data structure
+  * implemented via linked lists
+  */
+template<typename T>
+class TDisjointSetUnion2 {
+public:
+    typedef std::list<T> TList;
+    typedef std::vector< TList > TLists;
+
+    TLists Lists;
+public:
+    TDisjointSetUnion2() {
+    }
+
+    void MakeSet(const T &val) {
+        TList lst;
+        lst.push_back(val);
+        Lists.push_back(lst);
+    }
+
+    bool Union(int set1, int set2) {
+        if (set1 == set2)
+            return false;
+        TList &lst1 = Lists[set1];
+        TList &lst2 = Lists[set2];
+        for (typename TList::const_iterator iter = lst2.begin(); iter != lst2.end(); ++iter)
+            lst1.push_back(*iter);
+        lst2.clear();
+        return true;
+    }
+
+    int FindSet(const T &val) {
+        const int listsCnt = Lists.size();
+        for (int i = 0; i < listsCnt; ++i) {
+            const TList &lst = Lists[i];
+            for (typename TList::const_iterator iter = lst.begin(); iter != lst.end(); ++iter) {
+                if (val == *iter)
+                    return i;
+            }
+        }
+        return -1;
+    }
+};
 } // namespace ystd
