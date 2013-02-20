c0fe1bf0f5e385d34e26927274cff9c313d65e14
Yuliy Ayvazyan <aivyulik@gmail.com>
2013-02-21 02:24:01 +0400

    CodeForces 6B solution

--- codeforces/006B/main.cpp
+++ codeforces/006B/main.cpp
@@ -10,6 +10,49 @@
 using namespace std;
 
 int main() {
+    int n, m;
+    char pc;
+    scanf("%d %d %c", &n, &m, &pc);
 
+    char plan[100][100];
+    for (int i = 0; i < n; ++i) {
+        scanf("%s", plan[i]);
+    }
+
+    int left = 1000;
+    int right = -1000;
+    int top = 1000;
+    int bottom = -1000;
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            if (plan[i][j] == pc) {
+                left = min(left, j);
+                right = max(right, j);
+                top = min(top, i);
+                bottom = max(bottom, i);
+            }
+        }
+    }
+
+    set<char> colors;
+
+    for (int i = top; i <= bottom; ++i) {
+        if (left > 0)
+            colors.insert(plan[i][left-1]);
+
+        if (right < (m-1))
+            colors.insert(plan[i][right+1]);
+    }
+
+    for (int i = left; i <= right; ++i) {
+        if (top > 0)
+            colors.insert(plan[top-1][i]);
+
+        if (bottom < (n-1))
+            colors.insert(plan[bottom+1][i]);
+    }
+
+    colors.erase('.');
+    printf("%d\n", (int)colors.size());
     return 0;
 }
