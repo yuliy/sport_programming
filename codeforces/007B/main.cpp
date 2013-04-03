fe09223a7f4ba02b6592d00149ddabf5bfbb4efd
Yuliy Ayvazyan <aivyulik@gmail.com>
2013-04-03 23:08:44 +0400

    @

--- codeforces/007B/main.cpp
+++ codeforces/007B/main.cpp
@@ -9,7 +9,50 @@
 #include <algorithm>
 using namespace std;
 
-int main() {
+struct TMemBlock {
+    int Ptr;
+    int Size;
 
+    TMemBlock()
+        : Ptr(0)
+        , Size(0) {
+    }
+
+    TMemBlock(int ptr, int size)
+        : Ptr(ptr)
+        , Size(size) {
+    }
+};
+
+class TMemManager {
+private:
+    list<TMemBlock> Blocks;
+private:
+    TMemManager(const TMemManager&);
+    TMemManager& operator=(const TMemManager&);
+public:
+    TMemManager();
+    int Alloc(int size);
+    void Erase(int ptr);
+    void Defragment();
+};
+
+TMemManager::TMemManager() {
+}
+
+int TMemManager::Alloc(int size) {
+
+}
+
+void TMemManager::Erase(int ptr) {
+
+}
+
+void TMemManager::Defragment() {
+
+}
+
+int main() {
+    
     return 0;
 }
