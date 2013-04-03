df2dec49b246bb472cfc2d1cc90b7ce9160a56b9
Yuliy Ayvazyan <aivyulik@gmail.com>
2013-04-03 23:48:40 +0400

    @

--- codeforces/007B/main.cpp
+++ codeforces/007B/main.cpp
@@ -24,35 +24,128 @@ struct TMemBlock {
     }
 };
 
+typedef list<TMemBlock> TMemBlocks;
+
 class TMemManager {
 private:
-    list<TMemBlock> Blocks;
+    TMemBlocks Blocks;
+    int MemSize;
 private:
+    TMemManager();
     TMemManager(const TMemManager&);
     TMemManager& operator=(const TMemManager&);
+private:
+    TMemBlocks::iterator FindBlockByPtr(int ptr) {
+        for (TMemBlocks::iterator iter = Blocks.begin(); iter != Blocks.end(); ++iter) {
+            if (iter->Ptr == ptr)
+                return iter;
+        }
+        return Blocks.end();
+    }
 public:
-    TMemManager();
+    explicit TMemManager(int memSize);
     int Alloc(int size);
-    void Erase(int ptr);
+    bool Erase(int ptr);
     void Defragment();
 };
 
-TMemManager::TMemManager() {
+TMemManager::TMemManager(int memSize)
+    : MemSize(memSize) {
 }
 
 int TMemManager::Alloc(int size) {
+    TMemBlocks::iterator pos = Blocks.begin();
+    for (; pos != Blocks.end(); ++pos) {
+        if (pos == Blocks.begin()) {
+            if (size < pos->Ptr)
+                break;
+        } else {
+            TMemBlocks::iterator prev = pos;
+            --prev;
 
+            const int gap = pos->Ptr - prev->Ptr - prev->Size;
+            if (gap >= size)
+                break;
+        }
+    }
+
+    if (pos != Blocks.end()) {
+        const int ptr = pos->Ptr + pos->Size + 1;
+        TMemBlock b(ptr, size);
+        Blocks.insert(pos, b);
+        return ptr;
+    }
+
+    const TMemBlock &last = Blocks.back();
+    const int gap = MemSize - last.Ptr - last.Size + 1;
+    if (gap >= size) {
+        const int ptr = last.Ptr + last.Size + 1;
+        TMemBlock b(ptr, size);
+        Blocks.push_back(b);
+        return ptr;
+    }
+
+    return 0;
 }
 
-void TMemManager::Erase(int ptr) {
-
+bool TMemManager::Erase(int ptr) {
+    TMemBlocks::iterator iter = FindBlockByPtr(ptr);
+    if (iter != Blocks.end()) {
+        Blocks.erase(iter);
+        return true;
+    }
+    return false;
 }
 
 void TMemManager::Defragment() {
+    if (Blocks.empty())
+        return;
 
+    Blocks.front().Ptr = 0;
+    if (Blocks.size() < 2)
+        return;
+
+    TMemBlocks::iterator prev = Blocks.begin();
+    TMemBlocks::iterator iter = prev;
+    ++iter;
+
+    for (; iter != Blocks.end(); prev=iter,++iter) {
+        iter->Ptr = prev->Ptr + prev->Size;
+    }
 }
 
 int main() {
-    
+    int t, m;
+    scanf("%d %d", &t, &m);
+    TMemManager mm(m);
+
+    for (int i = 0; i < t; ++i) {
+        char buf[32];
+        scanf("%s", buf);
+        const char fch = buf[0];
+        switch(fch) {
+        case 'a': {
+            int sz;
+            scanf("%d", &sz);
+            const int p = mm.Alloc(sz);
+            if (p == 0)
+                printf("NULL\n");
+            else
+                printf("%d", sz);
+        } break;
+        case 'e': {
+            int ptr;
+            scanf("%d", &ptr);
+            if (!mm.Erase(ptr))
+                printf("ILLEGAL_ERASE_ARGUMENT\n");
+        } break;
+        case 'd': {
+            mm.Defragment();
+        } break;
+        default: {
+        } break;
+        }
+    }
+
     return 0;
 }
