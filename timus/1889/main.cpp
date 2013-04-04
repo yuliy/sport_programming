45c773ccf8e94756b765a49662c51a00cfb27518
Yuliy Ayvazyan <aivyulik@gmail.com>
2013-04-04 21:12:52 +0400

    timus 1889 draft

---
+++ timus/1889/main.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <set>
+#include <list>
+#include <algorithm>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
