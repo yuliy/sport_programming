c493564d6ebdbccdde1aa00c4164dd2ae566de73
Yuliy Ayvazyan <aivyulik@gmail.com>
2013-04-15 00:41:41 +0400

    codeforces 129B draft

---
+++ codeforces/129B/main.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <cstring>
+#include <string>
+#include <map>
+#include <set>
+#include <list>
+#include <algorithm>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
