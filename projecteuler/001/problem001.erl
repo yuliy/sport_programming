d8501fe066fef2577e7e1d0b0381d5931b974906
Yuliy Ayvazyan <aivyulik@gmail.com>
2013-04-22 20:43:26 +0400

    projecteuler problem1 Erlang solution :)

--- projecteuler/001/problem001.erl
+++ projecteuler/001/problem001.erl
@@ -1,25 +1,13 @@
 -module(problem001).
--export([sum/1]).
+-export([solve/1]).
 
-sum(num) when num == 1 ->
-    1;
-sum(num) when num =/= 1 ->
-    num + sum(num-1).
+% solve routine calculates sum of all natural numbers <= N which are multiples of 3 or 5
 
-sum(num) ->
-    case of num
-        1 -> 1;
-        _ -> num + sum(num-1).
-    
-
-
-% solve routine calculates sum of all natural numbers <= num which are multiples of 3 or 5
-
-%solve(num) when num == 0 ->
-%    0;
-%solve(num) when num rem 3 == 0 ->
-%    1 + solve(num-1);
-%solve(num) when num rem 5 == 0 ->
-%    1 + solve(num-1);
-%solve(num) ->
-%    solve(num-1).
+solve(N) when N == 0 ->
+    0;
+solve(N) when N rem 3 == 0 ->
+    N + solve(N-1);
+solve(N) when N rem 5 == 0 ->
+    N + solve(N-1);
+solve(N) ->
+    solve(N-1).
