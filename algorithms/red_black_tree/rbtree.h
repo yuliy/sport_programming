691cf6fe14eea40c1c6128ece7ea95ed734f7432
Yuliy Ayvazyan <aivyulik@gmail.com>
2013-04-27 17:31:54 +0400

    @

--- algorithms/red_black_tree/rbtree.h
+++ algorithms/red_black_tree/rbtree.h
@@ -6,24 +6,110 @@ enum EColour {
 };
 
 template<typename T>
-struct TNode {
-    EColour Colour;
-    TNode *Parent, *Left, *Right;
-    T Key;
+class TRBTree {
+private:
+    TNode *Root;
+private:
+    TRBTree(const TRBTree&); // TODO
+    TRBTree& operator=(const TRBTree&); // TODO
+public:
+    //template<typename T>
+    struct TNode {
+        EColour Colour;
+        TNode *Parent, *Left, *Right;
+        T Key;
 
-    TNode()
-        : Colour(C_BLACK)
-        , Parent(0)
-        , Left(0)
-        , Right(0)
-        , Key(T()) {
+        TNode()
+            : Colour(C_BLACK)
+            , Parent(0)
+            , Left(0)
+            , Right(0)
+            , Key(T()) {
+        }
+
+        TNode(EColour colour, TNode *parent, TNode *left, TNode *right, const T &key)
+            : Colour(colour)
+            , Parent(parent)
+            , Left(left)
+            , Right(right)
+            , Key(key) {
+        }
+    };
+public:
+    TRedBlackTree() : Root(0) {}
+
+    /**
+      * Searches node with a key given in a sub-tree with root x
+      */
+    const TNode* Search(const TNode *x, const T& key) const {
+        while ((x != NULL) && (key != x->Key))
+            x = (key < x->Key) ? x->Left : x->Right;
+        return x;
     }
 
-    TNode(EColour colour, TNode *parent, TNode *left, TNode *right, const T &key)
-        : Colour(colour)
-        , Parent(parent)
-        , Left(left)
-        , Right(right)
-        , Key(key) {
+    /**
+      * Returns node with a minimum key in a sub-tree with root x
+      */
+    const TNode *Minimum(const TNode *x) const {
+        if (x != NULL)
+            while (x->Left != NULL)
+                x = x->Left;
+        return x;
+    }
+
+    /**
+      * Returns node with a maximum key in a sub-tree with root x
+      */
+    const TNode *Maximum(const TNode *x) const {
+        if (x != NULL)
+            while (x->Right != NULL)
+                x = x->Right;
+        return x;
+    }
+
+    /**
+      * Returns successor of node x in inorder-tree-walk
+      */
+    const TNode *Successor(const TNode *x) const {
+        if (x == NULL)
+            return NULL;
+        if (x->Right != NULL)
+            return Minimum(x->Right);
+        const TNode *y = x->Parent;
+        while ((y != NULL) && (x == y->Right)) {
+            x = y;
+            y = y->Parent;
+        }
+        return y;
+    }
+
+    /**
+      * Returns predecessor of node x in inorder-tree-walk
+      */
+    const TNode *Predecessor(const TNode *x) const {
+        if (x == NULL)
+            return NULL;
+        if (x->Left != NULL)
+            return Maximum(x->Left);
+        const TNode *y = x->Parent;
+        while ((y != NULL) && (x == y->Left)) {
+            x = y;
+            y = y->Parent;
+        }
+        return y;
+    }
+
+    /**
+      *
+      */
+    void Insert(TNode *z) {
+        // TODO
+    }
+
+    /**
+      * Delete
+      */
+    void Delete(TNode *z) {
+        // TODO
     }
 };
