2dde1a518c2ddae00c5ccd3cb2e8ef6141359486
Yuliy Ayvazyan <aivyulik@gmail.com>
2013-04-27 16:04:06 +0400

    @

--- algorithms/red_black_tree/main.cpp
+++ algorithms/red_black_tree/main.cpp
@@ -8,74 +8,15 @@
 #include <iterator>
 #include <iomanip>
 #include <cmath>
+
 #include "rbtree.h"
+#include "tools.h"
+
 using namespace std;
-
-typedef unsigned int ui32;
-typedef long long i64;
-
-i64 GetTickCount() {
-    return clock() * 1000 / CLOCKS_PER_SEC;
-}
-
-static void FillWithData(TRBTree<int> &t) {
-    const int cnt = 15;
-    for (int i = 0; i < cnt; ++i) {
-        cout << "inserting: " << i << endl;
-        TNode<int> *n = new TNode<int>();
-        n->Key = i;
-        t.Insert(n);
-    }
-}
-
-static void PrintAllTreeStructure(TRBTree<int> &t) {
-    deque< TNode<int>* > q;
-    q.push_back(t.Root);
-    while (!q.empty()) {
-        TNode<int> *n = q.front();
-        q.pop_front();
-
-        cout << "(" << n->Key << ")\t"
-            << "c=" << ((n->Colour == C_BLACK) ? "B" : "R") << "\t"
-            << "p=" << (n->Parent ? n->Parent->Key : -1) << "\t"
-            << "l=" << (n->Left ? n->Left->Key : -1) << "\t"
-            << "r=" << (n->Right ? n->Right->Key : -1) << endl;
-
-        if (n->Left)
-            q.push_back(n->Left);
-        if (n->Right)
-            q.push_back(n->Right);
-    }
-}
-
-static void PrintTree(TRBTree<int> &t) {
-    TNode<int> *x = t.Minimum(t.Root);
-    while (x) {
-        cout << x->Key << endl;
-        x = t.Successor(x);
-    }
-}
-
-static void Test() {
-    TRBTree<int> t;
-
-    FillWithData(t);
-    PrintAllTreeStructure(t);
-    PrintTree(t);
-
-    // deleting element
-    TNode<int> *n1 = t.Search(t.Root, 8);
-    TNode<int> *n2 = t.Search(t.Root, 11);
-    cout << "n1->Key = " << n1->Key << endl;
-    t.Delete(n1);
-
-    PrintTree(t);
-}
 
 int main( int argc, char** argv ) {
     try {
-        //srand( time(NULL) );
-        Test();
+        //
     } catch (const exception &xcp) {
         cout << "An std::exception occured in main routine: " << xcp.what() << endl;
     } catch (...) {
