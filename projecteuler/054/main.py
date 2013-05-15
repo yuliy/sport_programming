401efa45c68e680d56fdb6e641726584d6c63928
Yuliy Ayvazyan <aivyulik@gmail.com>
2013-05-16 01:39:31 +0400

    @

--- projecteuler/054/main.py
+++ projecteuler/054/main.py
@@ -3,21 +3,38 @@
 """
 In the card game poker, a hand consists of five cards and are ranked, from lowest to highest, in the following way:
 
-High Card: Highest value card.
-One Pair: Two cards of the same value.
-Two Pairs: Two different pairs.
-Three of a Kind: Three cards of the same value.
-Straight: All cards are consecutive values.
-Flush: All cards of the same suit.
-Full House: Three of a kind and a pair.
-Four of a Kind: Four cards of the same value.
-Straight Flush: All cards are consecutive values of same suit.
-Royal Flush: Ten, Jack, Queen, King, Ace, in same suit.
+High Card:          Highest value card.
+One Pair:           Two cards of the same value.
+Two Pairs:          Two different pairs.
+Three of a Kind:    Three cards of the same value.
+Straight:           All cards are consecutive values.
+Flush:              All cards of the same suit.
+Full House:         Three of a kind and a pair.
+Four of a Kind:     Four cards of the same value.
+Straight Flush:     All cards are consecutive values of same suit.
+Royal Flush:        Ten, Jack, Queen, King, Ace, in same suit.
 The cards are valued in the order:
 2, 3, 4, 5, 6, 7, 8, 9, 10, Jack, Queen, King, Ace.
 """
 
-def Is
+name2cost = {
+    '2': 2,
+    '3': 3,
+    '4': 4,
+    '5': 5,
+    '6': 6,
+    '7': 7,
+    '8': 8,
+    '9': 9,
+    'T': 10,
+    'J': 11,
+    'Q': 12,
+    'K': 13,
+    'A': 14,
+}
+
+def convert_hand(hand):
+    return [(name2cost[card[0]], card[1]) for card in hand]
 
 def main():
     ifile = open('poker.txt', 'r')
@@ -28,6 +45,11 @@ def main():
 
         print '-----------------'
         print 'src="%s"' % line.split('\n')[0]
+        print 'hand1: %r' % hand1
+        print 'hand2: %r' % hand2
+
+        hand1 = convert_hand(hand1)
+        hand2 = convert_hand(hand2)
         print 'hand1: %r' % hand1
         print 'hand2: %r' % hand2
 
