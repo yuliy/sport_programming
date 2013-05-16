da13fe369bc152ae18116a307ddb972aa06b9252
Yuliy Ayvazyan <aivyulik@gmail.com>
2013-05-17 02:51:32 +0400

    @

--- projecteuler/054/somebodys_short_solution.py
+++ projecteuler/054/somebodys_short_solution.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
 
+"""
+I've found this solution here: http://www.s-anand.net/euler.html
+"""
+
 value = { '2':2,'3':3,'4':4,'5':5,'6':6,'7':7,'8':8,'9':9,'T':10,'J':11,'Q':12,'K':13,'A':14 }
 all_kinds = tuple(reversed(sorted(value.values())))
 all_suits = list('DCSH')
