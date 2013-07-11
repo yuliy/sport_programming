b136ba6869f167db4f9f6a250648d832b64116ac
Yuliy Ayvazyan <aivyulik@gmail.com>
2013-07-11 22:07:30 +0400

    @

--- timus/1930/main.cpp
+++ timus/1930/main.cpp
@@ -8,7 +8,18 @@
 #include <algorithm>
 using namespace std;
 
+typedef vector<int> TAdjList;
+typedef vector<TAdjList> TAdjLists;
+
 int main() {
+    int N, M;
+    scanf("%d %d", &N, &M);
+    TAdjLists adjLists(N);
+    for (int i = 0; i < M; ++i) {
+        int from, to;
+        scanf("%d %d", &from, &to);
+        adjLists[from].push_back(to);
+    }
 
     return 0;
 }
