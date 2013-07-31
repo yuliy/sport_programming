9c9bb821c8f7307b34b6b3f30cba975cb8c089cb
Yuliy Ayvazyan <aivyulik@gmail.com>
2013-07-31 21:03:44 +0400

    @

--- timus/1903/main.cpp
+++ timus/1903/main.cpp
@@ -8,7 +8,39 @@
 #include <algorithm>
 using namespace std;
 
+typedef unsigned int ui32;
+typedef long long i64;
+typedef unsigned long long ui64;
+
+const size_t MAX_N = 5000;
+const ui64 MOD = 1e9 + 7;
+
+int n, t;
+int k, x;
+int c[MAX_N+1];
+
+ui64 fact[MAX_N + 1];
+ui64 inv_fact[MAX_N + 1];
+
+static ui64 Pow(ui64 num) {
+
+}
+
+static void Init() {
+    fact[0] = 1;
+    for (int i = 1; i <= MAX_N; ++i) {
+        fact[i] = fact[i-1] * i;
+    }
+}
+
 int main() {
+    scanf("%d\t%d", &n, &t);
+    vector<int> c(n);
+    for (int i = 0; i < n; ++i)
+        scanf("%d", &c[i]);
+    scanf("%d\t%d", &k, &x);
+
+    Init();
 
     return 0;
 }
