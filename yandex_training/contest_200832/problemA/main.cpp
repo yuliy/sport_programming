ff62ad99745966ce37318657bfee34257c095919
Yuliy Ayvazyan <aivyulik@gmail.com>
2014-06-14 20:15:48 +0400

    @

--- yandex_training/contest_200832/problemA/main.cpp
+++ yandex_training/contest_200832/problemA/main.cpp
@@ -8,9 +8,31 @@
 #include <set>
 #include <list>
 #include <algorithm>
+#include <cctype>
 using namespace std;
 
 int main() {
+    string s;
+    cin >> s;
+    int capCount = 0;
+    for (auto ch : s) {
+        if (isupper(ch))
+            ++capCount;
+    }
 
+    int left = 0;
+    int right = capCount;
+    int res = capCount;
+    for (auto ch : s) {
+        if (isupper(ch)) {
+            --right;
+        } else {
+            ++left;
+        }
+
+        res = min(res, right + left);
+    }
+
+    cout << res << endl;
     return 0;
 }
