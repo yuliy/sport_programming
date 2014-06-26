20e24804ed42da102b12da1656afb4d346a323a0
Yuliy Ayvazyan <aivyulik@gmail.com>
2014-06-26 20:22:36 +0400

    @

--- yandex_training/contest_200955/problemA/main.cpp
+++ yandex_training/contest_200955/problemA/main.cpp
@@ -11,6 +11,39 @@
 using namespace std;
 
 int main() {
+    int n, m;
+    cin >> n >> m;
+    vector<int> a(m);
+    vector<int> b(n);
+    vector<string> field(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> field[i];
+        for (int j = 0; j < m; ++j) {
+            if ('*' == field[i][j]) {
+                ++a[j];
+                ++b[i];
+            }
+        }
+    }
+
+    long long res = 0;
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            if ('*' != field[i][j])
+                continue;
+
+            if (a[j] <= 1)
+                continue;
+
+            if (b[i] <= 1)
+                continue;
+
+            const long long cur_res = (a[j] - 1) * (b[i] - 1);
+            res += cur_res;
+        }
+    }
+
+    cout << res << endl;
 
     return 0;
 }
