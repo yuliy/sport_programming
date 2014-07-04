f97a5be6b9a3ddd3a55b70216106447210282f84
Yuliy Ayvazyan <aivyulik@gmail.com>
2014-07-04 10:26:36 +0400

    @

--- yandex_training/contest_200995/problemB/main.cpp
+++ yandex_training/contest_200995/problemB/main.cpp
@@ -8,11 +8,33 @@
 #include <set>
 #include <list>
 #include <algorithm>
+#include <cmath>
 using namespace std;
 
 int main() {
     int k, n;
     cin >> n >> k;
-    cout << (5 / 2) << endl;
+    const int pairs_cnt = n / 2;
+    if (k < pairs_cnt) {
+        cout << "-1" << endl;
+        return 0;
+    }
+
+    /*
+    int num = 1;
+    for (int i = 1; i < pairs_cnt; ++i) {
+        cout << num << " ";
+        num = GetNextNum(num);
+        cout << num << " ";
+        num = GetNextNum(num);
+    }
+    for (int i = 1; i < pairs_cnt; ++i)
+        cout << "1 1 ";
+    int tmp = k - pairs_cnt + 1;
+    cout << tmp << " " << tmp << " ";
+    if (1 == n % 2)
+        cout << "1" << endl;
+    */
+
     return 0;
 }
