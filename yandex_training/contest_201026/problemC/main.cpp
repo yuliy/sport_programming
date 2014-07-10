a5de3b34f89b994829feab85a12ee9d9e0321055
Yuliy Ayvazyan <aivyulik@gmail.com>
2014-07-10 20:52:39 +0400

    @

---
+++ yandex_training/contest_201026/problemC/main.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <deque>
+#include <cstring>
+#include <string>
+#include <map>
+#include <set>
+#include <list>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int n, k;
+    cin >> n >> k;
+    if (n == k) {
+        cout << "-1" << endl;
+        return 0;
+    }
+
+    printf("%d ", k);
+    for (int i = 2; i <= k; ++i) {
+        printf("%d ", i-1);
+    }
+    for (int i = k+1; k <= n; ++i) {
+        printf("%d", i);
+    }
+
+    return 0;
+}
