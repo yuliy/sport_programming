29f2a77118d7069e0d46adecbdc00a43c39786ce
Yuliy Ayvazyan <aivyulik@gmail.com>
2014-07-10 20:32:39 +0400

    @

--- yandex_training/contest_201026/problemA/main.cpp
+++ yandex_training/contest_201026/problemA/main.cpp
@@ -8,9 +8,32 @@
 #include <set>
 #include <list>
 #include <algorithm>
+#include <numeric>
 using namespace std;
 
 int main() {
+    int n;
+    cin >> n;
+    vector<int> x(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> x[i];
+    }
 
+    for (;;) {
+        bool nothingToChange = true;
+        sort(x.begin(), x.end());
+
+        for (int i = n - 1; i > 0; --i) {
+            if (x[i] > x[i-1]) {
+                x[i] -= x[i-1];
+                nothingToChange = false;
+            }
+        }
+
+        if (nothingToChange)
+            break;
+    }
+
+    cout << accumulate(x.begin(), x.end(), 0) << endl;
     return 0;
 }
