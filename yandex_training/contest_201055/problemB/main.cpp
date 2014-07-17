b2c263f245a51008560d52c878e9b8f5cd9670b3
Yuliy Ayvazyan <aivyulik@gmail.com>
2014-07-17 22:27:31 +0400

    @

--- yandex_training/contest_201055/problemB/main.cpp
+++ yandex_training/contest_201055/problemB/main.cpp
@@ -15,6 +15,32 @@ int main() {
     cin >> s;
     const int n = s.size();
 
+    typedef set<char> TDict;
+    TDict dict;
+    for (int i = 0; i < n; ++i) {
+        dict.insert(s[i]);
+    }
+
+    int pos = 0;
+    const char min_ch = *dict.begin();
+    for (TDict::const_reverse_iterator iter = dict.rbegin(); iter != dict.rend(); ++iter) {
+        const char ch = *iter;
+        if (ch == min_ch) {
+            cout << ch << endl;
+            break;
+        }
+
+        for (int i = pos; i < n; ++i) {
+            if (s[i] == *iter) {
+                cout << ch;
+                pos = i;
+            }
+        }
+
+        ++pos;
+        if (pos >=n)
+            break;
+    }
 
     return 0;
 }
