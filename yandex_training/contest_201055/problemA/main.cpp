c131f247e530f7418e733c51866a6e4e812096e9
Yuliy Ayvazyan <aivyulik@gmail.com>
2014-07-17 20:27:58 +0400

    @

--- yandex_training/contest_201055/problemA/main.cpp
+++ yandex_training/contest_201055/problemA/main.cpp
@@ -11,6 +11,29 @@
 using namespace std;
 
 int main() {
+    int n;
+    cin >> n;
+    int x = 0;
+    int y = n / 7 + 1;
 
+    for (; y >= 0;) {
+        const int tmp = 4 * x + 7 * y;
+        if (tmp == n) {
+            for (; x > 0; --x)
+                cout << 4;
+            for (; y > 0; --y)
+                cout << 7;
+            cout << endl;
+            return 0;
+        }
+
+        if (tmp > n) {
+            --y;
+        } else {
+            ++x;
+        }
+    }
+
+    cout << "-1" << endl;
     return 0;
 }
