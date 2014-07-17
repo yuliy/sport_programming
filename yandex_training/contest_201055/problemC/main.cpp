bf5213a1cef9e2f12f12780df3dac484f0251f66
Yuliy Ayvazyan <aivyulik@gmail.com>
2014-07-17 21:11:44 +0400

    @

--- yandex_training/contest_201055/problemC/main.cpp
+++ yandex_training/contest_201055/problemC/main.cpp
@@ -28,17 +28,23 @@ int main() {
         int prev_first = first;
         int prev_last = last;
         if (sum > t) {
-            sum -= a[first];
-            if (first < last)
+            if (first < last || (first == last && last < max_last)) {
+                sum -= a[first];
                 ++first;
+            }
         } else if (sum < t) {
-            sum += a[last];
-            if (last < max_last)
+            res = max(res, last - first + 1);
+            if (last < max_last) {
                 ++last;
+                sum += a[last];
+            }
         } else {
             res = max(res, last - first + 1);
+            if (first < last)
+                ++first;
         }
 
+        //cout << first << "\t" << last << endl;
         if (prev_first == first && prev_last == last)
             break;
     }
