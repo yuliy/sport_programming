acd57adf02227c3c3628da7b6dfd90a39aae438c
Yuliy Ayvazyan <aivyulik@gmail.com>
2014-07-24 20:16:32 +0400

    @

--- yandex_training/contest_201075/problemA/main.cpp
+++ yandex_training/contest_201075/problemA/main.cpp
@@ -11,6 +11,15 @@
 using namespace std;
 
 int main() {
+    int n, m;
+    cin >> n >> m;
+    vector<int> a(n);
+    for (int i = 0; i < n; ++i)
+        scanf("%d", &a[i]);
+
+    for (int i = 0; i < m; ++i) {
+        //
+    }
 
     return 0;
 }
