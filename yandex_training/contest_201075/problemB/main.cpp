079137d65da23a9b6eee4acda65fc9cf8c1e7155
Yuliy Ayvazyan <aivyulik@gmail.com>
2014-07-24 20:55:54 +0400

    @

--- yandex_training/contest_201075/problemB/main.cpp
+++ yandex_training/contest_201075/problemB/main.cpp
@@ -10,7 +10,58 @@
 #include <algorithm>
 using namespace std;
 
-int main() {
+enum EState {
+    ST_NONE,
+    ST_IN_SEQUENCE,
+};
 
+int main() {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; ++i) {
+        scanf("%d", &a[i]);
+    }
+
+    int global_res = 0;
+    for (;;) {
+        vector<int> newa;
+        n = a.size();
+        int res = 0;
+        int cur_len = 0;
+        EState state = ST_NONE;
+        for (int i = 1; i < n; ++i) {
+            switch(state) {
+            case ST_NONE: {
+                    if (a[i-1] > a[i]) {
+                        state = ST_IN_SEQUENCE;
+                        cur_len = 1;
+                        newa.push_back(a[i-1]);
+                    }
+                } break;
+            case ST_IN_SEQUENCE: {
+                    if (a[i-1] < a[i]) {
+                        ++cur_len;
+                    } else {
+                        res = max(cur_len, res);
+                        state = ST_NONE;
+                    }
+                } break;
+            }
+        }
+
+        if (state == ST_IN_SEQUENCE) {
+            res = max(cur_len, res);
+        }
+
+        if (newa.size() == a.size())
+            break;
+
+        global_res += res;
+        a.swap(newa);
+        //cout << res << endl;
+    }
+
+    cout << global_res << endl;
     return 0;
 }
