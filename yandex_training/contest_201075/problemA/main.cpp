2901f8f180169c9446516774b3397a4dcc82bbc5
Yuliy Ayvazyan <aivyulik@gmail.com>
2014-07-24 20:23:23 +0400

    @

--- yandex_training/contest_201075/problemA/main.cpp
+++ yandex_training/contest_201075/problemA/main.cpp
@@ -17,8 +17,24 @@ int main() {
     for (int i = 0; i < n; ++i)
         scanf("%d", &a[i]);
 
+    vector<int> res(n);
+    res[n-1] = 1;
+    set<int> dict;
+    dict.insert(a[n-1]);
+    for (int i = n - 2; i >= 0; --i) {
+        set<int>::const_iterator iter = dict.find(a[i]);
+        if (iter != dict.end()) {
+            res[i] = res[i+1];
+        } else {
+            res[i] = res[i+1] + 1;
+            dict.insert(a[i]);
+        }
+    }
+
     for (int i = 0; i < m; ++i) {
-        //
+        int li = 0;
+        scanf("%d", &li);
+        printf("%d\n", res[li-1]);
     }
 
     return 0;
