81b5b002451b7fb354a796ae8207508fb989ddfe
Yuliy Ayvazyan <aivyulik@gmail.com>
2014-07-27 01:08:47 +0400

    codeforces 451A solution

--- codeforces/451A/main.cpp
+++ codeforces/451A/main.cpp
@@ -11,6 +11,11 @@
 using namespace std;
 
 int main() {
-
+    int n, m;
+    cin >> n >> m;
+    if (min(n, m) % 2)
+        cout << "Akshat" << endl;
+    else
+        cout << "Malvika" << endl;
     return 0;
 }
