83c45d7873bf7c3f76ab0bbd20d8a7f22eb2c191
Yuliy Ayvazyan <aivyulik@gmail.com>
2014-07-31 21:22:52 +0400

    @

--- yandex_training/contest_201102/problemA/main.cpp
+++ yandex_training/contest_201102/problemA/main.cpp
@@ -10,10 +10,13 @@
 #include <algorithm>
 using namespace std;
 
+//#define DEBUG
+
 typedef vector< pair<int, int> > TAdjList;
 typedef vector< TAdjList > TAdjLists;
 
 static void PrintGraph(const TAdjLists &g) {
+    #ifdef DEBUG
     const int n = g.size() - 1;
     for (int from = 1; from <= n; ++from) {
         cout << from << ": ";
@@ -25,6 +28,7 @@ static void PrintGraph(const TAdjLists &g) {
         }
         cout << endl;
     }
+    #endif
 }
 
 int main() {
@@ -35,7 +39,8 @@ int main() {
         int from, to, cost;
         scanf("%d %d %d", &from, &to, &cost);
         adjLists[from].push_back( make_pair(to, cost) );
-        adjLists[to].push_back( make_pair(from, -cost) );
+        if (from != 1 | adjLists[from].size() > 1)
+            adjLists[to].push_back( make_pair(from, -cost) );
     }
 
     PrintGraph(adjLists);
@@ -44,24 +49,38 @@ int main() {
     int nc = 0;
     int cur_vertex = 1;
     vector<bool> visited(n+1);
+    //visited[cur_vertex] = true;
     for (int i = 1; i <= n; ++i) {
         const TAdjList &lst = adjLists[cur_vertex];
+        bool found = false;
         for (TAdjList::const_iterator iter = lst.begin(); iter != lst.end(); ++iter) {
             const int to_vertex = iter->first;
             const int cost = iter->second;
+            #ifdef DEBUG
+            cout << "[" << to_vertex << ", " << cost << "]" << endl;
+            #endif
             if (visited[to_vertex])
                 continue;
 
+            found = true;
             if (cost > 0) {
                 pc += cost;
             } else {
                 nc -= cost;
             }
 
+            #ifdef DEBUG
             cout << cur_vertex << "\t" << to_vertex << "\t" << cost << "\t(pc=" << pc << "; nc=" << nc << ")" << endl;
+            #endif
             visited[to_vertex] = true;
             cur_vertex = to_vertex;
+            break;
         }
+
+        #ifdef DEBUG
+        if (!found)
+            cout << "Achtung! Not found!" << endl;
+        #endif
     }
 
     cout << min(pc, nc) << endl;
