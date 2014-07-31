c1d152f229f7859015f9a7d053dbcb396f5bf7cf
Yuliy Ayvazyan <aivyulik@gmail.com>
2014-08-01 00:59:38 +0400

    @

--- yandex_training/contest_201102/problemC/main.cpp
+++ yandex_training/contest_201102/problemC/main.cpp
@@ -30,25 +30,38 @@ static void PrintGraph(const TAdjLists &g) {
     #endif
 }
 
-static void DFS_Visit(const TAdjLists &g, vector<bool> &visited, int u) {
+static bool DFS_Visit(const TAdjLists &g, vector<bool> &visited, int u, vector<int> &cycle) {
     visited[u] = true;
-    cout << u << endl;
+    cycle.push_back(u);
+    cout << "(" << u << ")" << endl;
     const TAdjList &lst = g[u];
     for (TAdjList::const_iterator iter = lst.begin(); iter != lst.end(); ++iter) {
         const int v = *iter;
         if (!visited[v]) {
-            DFS_Visit(g, visited, v);
+            if (DFS_Visit(g, visited, v, cycle)) {
+                return true;
+            }
+        } else {
+            cycle.push_back(v);
+            return true;
         }
     }
+
+    cycle.pop_back();
+    return false;
 }
 
-static void DFS(const TAdjLists &g, vector<bool> &visited) {
+static void DFS(const TAdjLists &g, vector<bool> &visited, vector<int> &cycle) {
     const int n = g.size() - 1;
     for (int u = 1; u <= n; ++u) {
         if (!visited[u]) {
-            DFS_Visit(g, visited, u);
+            cycle.clear();
+            if (DFS_Visit(g, visited, u, cycle)) {
+                return;
+            }
         }
     }
+    cycle.clear();
 }
 
 int main() {
@@ -65,6 +78,10 @@ int main() {
     PrintGraph(g);
 
     vector<bool> visited(n + 1);
-    DFS(g, visited);
+    vector<int> cycle;
+    DFS(g, visited, cycle);
+    for (vector<int>::const_iterator iter = cycle.begin(); iter != cycle.end(); ++iter) {
+        cout << *iter << endl;
+    }
     return 0;
 }
