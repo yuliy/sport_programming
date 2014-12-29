0226e729e34717ca463edcdf372160bef145f481
Yuliy Ayvazyan <aivyulik@gmail.com>
2014-12-29 21:16:07 +0300

    @

--- codeforces/vcontest_499/B/main.cpp
+++ codeforces/vcontest_499/B/main.cpp
@@ -11,6 +11,24 @@
 using namespace std;
 
 int main() {
+    int n, m;
+    cin >> n >> m;
+
+    map<string, string> dict;
+    for (int i = 0; i < m; ++i) {
+        string a, b;
+        cin >> a >> b;
+        dict[a] = (a.size() <= b.size()) ? a : b;
+    }
+
+    for (int i = 0; i < n; ++i) {
+        string c;
+        cin >> c;
+        if (i != (n-1))
+            cout << dict[c] << ' ';
+        else
+            cout << dict[c] << endl;
+    }
 
     return 0;
 }
