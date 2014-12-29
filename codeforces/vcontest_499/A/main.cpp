6406a727da416c833b4122cf1ea8be76385fb6be
Yuliy Ayvazyan <aivyulik@gmail.com>
2014-12-29 20:20:53 +0300

    @

--- codeforces/vcontest_499/A/main.cpp
+++ codeforces/vcontest_499/A/main.cpp
@@ -11,6 +11,11 @@
 using namespace std;
 
 int main() {
-
+    int n, x;
+    scanf("%d %d", &n, &x);
+    vector< pair<int, int> > moments(n);
+    for (int i = 0; i < n; ++i) {
+        scanf("%d %d", &moments[i].first, &moments[i].second);
+    }
     return 0;
 }
