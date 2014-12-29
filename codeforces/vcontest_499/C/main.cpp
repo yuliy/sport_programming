ad400bb893a7b42bf86ece91e5c0da3ba31560d8
Yuliy Ayvazyan <aivyulik@gmail.com>
2014-12-29 22:04:58 +0300

    @

--- codeforces/vcontest_499/C/main.cpp
+++ codeforces/vcontest_499/C/main.cpp
@@ -11,6 +11,20 @@
 using namespace std;
 
 int main() {
+    int x0, y0, x1, y1;
+    scanf("%d %d %d %d", &x0, &y0, &x1, &y1);
 
+    int N;
+    scanf("%d", &N);
+    int res = 0;
+    for (int i = 0; i < N; ++i) {
+        int a, b, c;
+        scanf("%d %d %d", &a, &b, &c);
+
+        const int t = (a * x0 + b * y0 + c) * (a * x1 + b * y1 + c);
+        if (t < 0)
+            ++res;
+    }
+    cout << res << endl;
     return 0;
 }
