128befc15f75ce9bb487f24d69004567afafaab4
Yuliy Ayvazyan <aivyulik@gmail.com>
2015-03-09 02:02:22 +0300

    @

--- codeforces/522/A/main.cpp
+++ codeforces/522/A/main.cpp
@@ -7,15 +7,51 @@
 #include <map>
 #include <set>
 #include <list>
+#include <unordered_map>
 #include <algorithm>
+#include <locale>
 using namespace std;
 
+typedef vector<string> TReposters;
+typedef unordered_map<string, TReposters> TName2reposters;
+
+int dfs(const string& name, const TName2reposters& name2reposters, int curDepth) {
+    //cout << name << "\t" << curDepth << endl;
+    TName2reposters::const_iterator it = name2reposters.find(name);
+    if (it == name2reposters.end())
+        return curDepth;
+
+    const TReposters& reposters = it->second;
+    int res = curDepth;
+    for (const auto& reposter : reposters) {
+        res = max(curDepth, dfs(reposter, name2reposters, curDepth + 1));
+    }
+    return res;
+}
+
+void make_lower(string& s) {
+    static std::locale loc;
+    for (auto& ch : s) {
+        ch = tolower(ch);
+    }
+}
+
 int main() {
+
     int n;
     cin >> n;
+
+    TName2reposters name2reposters;
     for (int i = 0; i < n; ++i) {
         string name1, name2;
         cin >> name1 >> name2 >> name2;
+        make_lower(name1);
+        make_lower(name2);
+        //cout << name1 << ":" << name2 << endl;
+        name2reposters[name2].push_back(name1);
     }
+
+    cout << dfs("polycarp", name2reposters, 1) << endl;
+
     return 0;
 }
