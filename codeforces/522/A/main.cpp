4ad8c63ce032229adabf8560c98e96829582fb20
Yuliy Ayvazyan <aivyulik@gmail.com>
2015-03-09 01:34:33 +0300

    @

--- codeforces/522/A/main.cpp
+++ codeforces/522/A/main.cpp
@@ -11,6 +11,11 @@
 using namespace std;
 
 int main() {
-
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; ++i) {
+        string name1, name2;
+        cin >> name1 >> name2 >> name2;
+    }
     return 0;
 }
