ae90c4bd214d964eff2e75b55328728c894a7b29
Yuliy Ayvazyan <aivyulik@gmail.com>
2015-03-13 02:23:09 +0300

    @

---
+++ google_code_jam/2014_google_code_jam/qualification_round/main.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <deque>
+#include <cstring>
+#include <string>
+#include <map>
+#include <set>
+#include <list>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int numberOfTests = 0;
+    cin >> numberOfTests;
+
+    return 0;
+}
