61379292973b2deb5810be7533535291ada23371
Yuliy Ayvazyan <aivyulik@gmail.com>
2015-03-17 20:29:06 +0300

    @

--- codeforces/round296/B/main.cpp
+++ codeforces/round296/B/main.cpp
@@ -14,9 +14,14 @@ struct TFoo {
     int HemDist;
     bool AFlags[26];
     bool BFlags[26];
+    bool PairFound;
+    pair<int, int> Pair;
+    size_t LastDiffPairIdx;
 
     TFoo()
         : HemDist(0)
+        , PairFound(false)
+        , LastDiffPairIdx( static_cast<size_t>(-1) )
     {
         for (int i = 0; i < 26; ++i) {
             AFlags[i] = BFlags[i] = false;
@@ -27,19 +32,36 @@ struct TFoo {
 TFoo calc_hemming(const string& a, const string& b) {
     TFoo res;
     const size_t sz = a.size();
-    for (int i = 0; i < sz; ++i) {
-        if (a[i] != b[i]) {
-            res.HemDist += 1;
-        }
+    map<string, size_t> para2idx;
+    for (size_t i = 0; i < sz; ++i) {
+        if (a[i] == b[i])
+            continue;
+
+        res.LastDiffPairIdx = i;
+
+        res.HemDist += 1;
 
         res.AFlags[ a[i] - 'a' ] = true;
         res.BFlags[ b[i] - 'a' ] = true;
+
+        string p("  ");
+        p[0] = a[i];
+        p[1] = b[i];
+
+        map<string, size_t>::const_iterator it = para2idx.find(p);
+        if (it != para2idx.end()) {
+            res.PairFound = true;
+            res.Pair = make_pair(it->second, i);
+        } else {
+            string invp("  ");
+            p[0] = b[i];
+            p[1] = a[i];
+            para2idx.insert(make_pair(
+                invp, i
+            ));
+        }
     }
     return res;
-}
-
-bool check_has_common_letter(const string& a, const string& b) {
-
 }
 
 int main() {
@@ -51,6 +73,20 @@ int main() {
     if (res.HemDist <= 1) {
         cout << res.HemDist << endl
             << "-1 -1" << endl;
+        return 0;
+    }
+
+    if (res.PairFound) {
+        cout << (res.HemDist - 2) << endl
+            << res.Pair.first << " " << res.Pair.second << endl;
+        return 0;
+    }
+
+    for (int i = 0; i < 26; ++i) {
+        if (res.AFlags[i] && res.BFlags[i]) {
+            cout << (res.HemDist - 1) << endl
+                << i << " " << res.LastDiffPairIdx << endl;
+        }
     }
 
     return 0;
