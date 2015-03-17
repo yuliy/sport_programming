68599587aaf5589f809def40214d29fcbbe6dba9
Yuliy Ayvazyan <aivyulik@gmail.com>
2015-03-18 02:17:25 +0300

    @

--- codeforces/527/main.cpp
+++ codeforces/527/main.cpp
@@ -8,14 +8,35 @@
 #include <set>
 #include <list>
 #include <algorithm>
+#include <exception>
 using namespace std;
+
+typedef map<int, int> TStart2ID;
+typedef map<int, int, greater<int> > TLen2ID;
 
 class TSolver {
 private:
-    map<int, int> Start2ID;
-    map<int, int, greater<int> > Len2ID;
+    int Total;
+    TStart2ID Start2ID;
+    TLen2ID Len2ID;
+private:
+    TLen2ID::iterator FindInterval(int len, int id) {
+        auto range = Len2ID.equal_range(len);
+        for (
+            TLen2ID::iterator it = range.first;
+            it != range.second;
+            ++it
+        ) {
+            if (it->second == id) {
+                return it;
+            }
+        }
+
+        throw logic_error("FindInterval failed searching!");
+    }
 public:
     explicit TSolver(int total)
+        : Total(total)
     {
         Start2ID.insert(make_pair(
             0, total
@@ -25,11 +46,41 @@ public:
         ));
     }
 
-    void AddSplit(int x) {
-
+    int GetMax() const {
+        return Len2ID.begin()->first;
     }
 
-    //
+    void AddSplit(int x, int id) {
+        //
+        TStart2ID::iterator lb = Start2ID.lower_bound(x);
+        const int intervalStart = lb->first;
+        const int intervalID = lb->second;
+        ++lb;
+        const int intervalLen = (lb == Start2ID.end()) ? Total : (lb->first - intervalStart);
+        --lb;
+        if (lb == Start2ID.end())
+            throw logic_error("lb should be valid!");
+        Start2ID.erase(lb);
+
+        //
+        TLen2ID::iterator lenIter = FindInterval(intervalLen, intervalID);
+        Len2ID.erase(lenIter);
+
+        //
+        Start2ID.insert(make_pair(
+            intervalStart, intervalID
+        ));
+        Start2ID.insert(make_pair(
+            x, id
+        ));
+
+        Len2ID.insert(make_pair(
+            x - intervalStart, intervalID
+        ));
+        Len2ID.insert(make_pair(
+            intervalStart + intervalLen - x, id
+        ));
+    }
 };
 
 int main() {
@@ -43,11 +94,14 @@ int main() {
         int where;
         cin >> type >> where;
         if (type == "H") {
-            ysolver.AddSplit(where);
+            ysolver.AddSplit(where, i);
         } else {
-            xsolver.AddSplit(where);
+            xsolver.AddSplit(where, i);
         }
     }
+
+    const int res = xsolver.GetMax() * ysolver.GetMax();
+    cout << res << endl;
 
     return 0;
 }
--- codeforces/debug_build.sh
+++ codeforces/debug_build.sh
@@ -1,2 +1,3 @@
 #g++ main.cpp -Wall -g -o x
-g++-mp-4.7 main.cpp -Wall -std=c++11 -g -o x
+#g++-mp-4.7 main.cpp -Wall -std=c++11 -g -o x
+g++-4.8 -Wall -O3 -std=c++11 -g -o x main.cpp
