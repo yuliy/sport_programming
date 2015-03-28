deb09178c85842be90e5d34b71e488ee99a71f08
Yuliy Ayvazyan <aivyulik@gmail.com>
2015-03-28 17:01:11 +0300

    @

--- russian_code_cup/RCC2015/warmup_round/A/main.cpp
+++ russian_code_cup/RCC2015/warmup_round/A/main.cpp
@@ -5,12 +5,61 @@
 #include <cstring>
 #include <string>
 #include <map>
+#include <unordered_map>
 #include <set>
 #include <list>
 #include <algorithm>
 using namespace std;
 
+typedef unordered_map<int, int> TMap;
+
+void pr(int colour, int count) {
+    for (int i = 0; i < count; ++i) {
+        printf("%d ", colour);
+    }
+}
+
 int main() {
+    int n, k;
+    scanf("%d %d", &n, &k);
+    vector<int> a(n);
+    TMap colour2count;
+    for (int i = 0; i < n; ++i) {
+        int colour;
+        scanf("%d", &colour);
+        ++colour2count[colour];
+    }
+
+    int to_choose = k;
+    for (
+        TMap::iterator iter it = colour2count.begin();
+        it != colour2count.end();
+        ++it
+    ) {
+        if (0 == to_choose)
+            break;
+        pr(iter->first, 1);
+        it->second -= 1;
+        ++chosen;
+    }
+
+    for (
+        TMap::iterator iter it = colour2count.begin();
+        it != colour2count.end();
+        ++it
+    ) {
+        if (0 == to_choose)
+            break;
+        if (it->second <= to_choose) {
+            pr(it->first, it->second);
+            to_choose -= it->second;
+            it->second = 0;
+        } else {
+            pr(it->first, to_choose);
+            it->second -= to_choose;
+            to_choose = 0;
+        }
+    }
 
     return 0;
 }
