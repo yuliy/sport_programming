5b1ed1d4c8ed47a173d96f48988aa6b3efcf8b14
Yuliy Ayvazyan <aivyulik@gmail.com>
2015-04-12 01:33:02 +0300

    @

---
+++ google_code_jam/2015_google_code_jam/qualification_round/D/main.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <deque>
+#include <cstring>
+#include <string>
+#include <map>
+#include <set>
+#include <list>
+#include <algorithm>
+using namespace std;
+
+static const bool DBG = false;
+
+
+static string SolveSingleCase() {
+    return "";
+}
+
+int main() {
+    int numberOfTests = 0;
+    cin >> numberOfTests;
+
+    for (int i = 1; i <= numberOfTests; ++i) {
+        cout << "Case #" << i << ": " << SolveSingleCase() << endl;
+    }
+
+    return 0;
+}
