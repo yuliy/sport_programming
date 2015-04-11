bfe7749146523cd94913d595fbc79d19fb4a6715
Yuliy Ayvazyan <aivyulik@gmail.com>
2015-04-12 02:18:54 +0300

    @

--- google_code_jam/2015_google_code_jam/qualification_round/D/main.cpp
+++ google_code_jam/2015_google_code_jam/qualification_round/D/main.cpp
@@ -14,7 +14,27 @@ static const bool DBG = false;
 
 
 static string SolveSingleCase() {
-    return "";
+    int X, R, C;
+    scanf("%d %d %d", &X, &R, &C);
+    const int area = R * C;
+
+    if (1 == X) {
+        return "GABRIEL";
+    }
+
+    if (2 == X) {
+        return (area % 2) ? "RICHARD" : "GABRIEL";
+    }
+
+    if (3 == X) {
+        return ((area % 3 == 0) && (R != 1) && (C != 1)) ? "GABRIEL" : "RICHARD";
+    }
+
+    if (4 == X) {
+        return ((4 == R && 4 == C) || (3 == R && 4 == C) || (4 == R && 3 == C)) ? "GABRIEL" : "RICHARD";
+    }
+
+    throw string("ooops!");
 }
 
 int main() {
