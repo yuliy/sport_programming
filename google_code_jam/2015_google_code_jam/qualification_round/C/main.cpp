9a47b89700d95bc6f9dae7145c7d22b289e6cb2c
Yuliy Ayvazyan <aivyulik@gmail.com>
2015-04-11 22:35:14 +0300

    @

---
+++ google_code_jam/2015_google_code_jam/qualification_round/C/main.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <deque>
+#include <cstring>
+#include <string>
+#include <map>
+#include <set>
+#include <list>
+#include <algorithm>
+using namespace std;
+
+typedef map<int, int> TMap;
+
+static const bool DBG = false;
+
+static int SolveSingleCase() {
+
+    return res;
+}
+
+int main() {
+    int numberOfTests = 0;
+    cin >> numberOfTests;
+
+    for (int i = 1; i <= numberOfTests; ++i) {
+        cout << "Case #" << i << ": " << SolveSingleCase() << endl;
+    }
+
+    return 0;
+}
