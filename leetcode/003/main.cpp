67d17c57dcf0e382cbcb59715a63aba60a116ecd
Yuliy Ayvazyan <aivyulik@gmail.com>
2015-09-08 01:20:26 +0300

    @

--- leetcode/003/main.cpp
+++ leetcode/003/main.cpp
@@ -6,11 +6,25 @@
 #include <string>
 #include <map>
 #include <set>
+#include <unordered_set>
 #include <list>
 #include <algorithm>
 using namespace std;
 
-int main() {
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        if (s.empty())
+            return 0;
 
+    }
+};
+
+int main() {
+    string s;
+    cin >> s;
+
+    Solution solution;
+    cout << solution.lengthOfLongestSubstring(s) << endl;
     return 0;
 }
