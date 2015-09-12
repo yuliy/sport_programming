1f1380e032b09c3aff6cfaecb1706490a171699f
Yuliy Ayvazyan <aivyulik@gmail.com>
2015-09-12 03:02:16 +0300

    @

--- leetcode/003/main.cpp
+++ leetcode/003/main.cpp
@@ -7,6 +7,7 @@
 #include <map>
 #include <set>
 #include <unordered_set>
+#include <unordered_map>
 #include <list>
 #include <algorithm>
 using namespace std;
@@ -17,6 +18,27 @@ public:
         if (s.empty())
             return 0;
 
+        const int sz = s.size();
+        unordered_map<char, int> char2cnt;
+        char2cnt[s[0]] = 1;
+        int beg = 0;
+        int end = 1;
+
+        int bestResult = 1;
+        for (;;) {
+            while (0 == char2cnt[ s[end] ] && end < sz) {
+                char2cnt[ s[end] ] += 1;
+                ++end;
+            }
+            bestResult = max(bestResult, end-beg);
+
+            while (0 != char2cnt[ s[beg] ]) {
+                char2cnt[ s[beg] ] -= 1;
+                ++beg;
+            }
+        }
+
+        return bestResult;
     }
 };
 
