c6179c7545e368d7a95df618d029fd129387e1bd
Yuliy Ayvazyan <aivyulik@gmail.com>
2015-09-13 20:48:38 +0300

    @

--- leetcode/003/main.cpp
+++ leetcode/003/main.cpp
@@ -12,29 +12,32 @@
 #include <algorithm>
 using namespace std;
 
+
 class Solution {
+private:
+    bool HasDuplicates(const string& s, int beg, int end) const {
+        //cout << "beg=" << beg << '\t' << "end=" << end << endl;
+        bool dict[257] = {false};
+        for (int i = beg; i <= end; ++i) {
+            const int chIdx = static_cast<int>(s[i]) + 128;
+            if (dict[chIdx])
+                return true;
+            dict[chIdx] = true;
+        }
+        return false;
+    }
 public:
     int lengthOfLongestSubstring(string s) {
         if (s.empty())
             return 0;
 
         const int sz = s.size();
-        unordered_map<char, int> char2cnt;
-        char2cnt[s[0]] = 1;
-        int beg = 0;
-        int end = 1;
-
         int bestResult = 1;
-        for (;;) {
-            while (0 == char2cnt[ s[end] ] && end < sz) {
-                char2cnt[ s[end] ] += 1;
-                ++end;
-            }
-            bestResult = max(bestResult, end-beg);
-
-            while (0 != char2cnt[ s[beg] ]) {
-                char2cnt[ s[beg] ] -= 1;
-                ++beg;
+        for (int i = 0; i < sz; ++i) {
+            for (int j = i+1; j < sz; ++j) {
+                if (HasDuplicates(s, i, j))
+                    break;
+                bestResult = max(bestResult, j - i + 1);
             }
         }
 
@@ -47,6 +50,6 @@ int main() {
     cin >> s;
 
     Solution solution;
-    cout << solution.lengthOfLongestSubstring(s) << endl;
+    cout << solution.lengthOfLongestSubstring(s) << endl;;
     return 0;
 }
