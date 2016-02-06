553c772cc56a7fa9aa103989e0a7c854b27178a7
Yuliy Ayvazyan <yuliy@yandex-team.ru>
2016-02-06 21:18:03 +0300

    timus 2002 solution

--- timus/2002/main.cpp
+++ timus/2002/main.cpp
@@ -10,7 +10,77 @@
 #include <algorithm>
 using namespace std;
 
-int main() {
+class TApplication {
+public:
+    void Run() {
+        int n = 0;
+        cin >> n;
 
+        for (int i = 0; i < n; ++i) {
+            string command;
+            cin >> command;
+
+            if (command == "register") {
+                string username, password;
+                cin >> username >> password;
+                Register(username, password);
+            } else if (command == "login") {
+                string username, password;
+                cin >> username >> password;
+                Login(username, password);
+            } else {
+                string username;
+                cin >> username;
+                Logout(username);
+            }
+        }
+
+    }
+private:
+    unordered_map<string, string> Username2Password;
+    unordered_set<string> LoggedInUsers;
+private:
+    void Register(const string& username, const string& password) {
+        const auto it = Username2Password.find(username);
+        if (it != Username2Password.end()) {
+            cout << "fail: user already exists" << endl;
+        } else {
+            Username2Password.insert(make_pair(username, password));
+            cout << "success: new user added" << endl;
+        }
+    }
+
+    void Login(const string& username, const string& password) {
+        const auto it = Username2Password.find(username);
+        const auto lit = LoggedInUsers.find(username);
+        if (it == Username2Password.end()) {
+            cout << "fail: no such user" << endl;
+        } else if (it->second != password) {
+            cout << "fail: incorrect password" << endl;
+        } else if (lit != LoggedInUsers.end()) {
+            cout << "fail: already logged in" << endl;
+        } else {
+            LoggedInUsers.insert(username);
+            cout << "success: user logged in" << endl;
+        }
+    }
+
+    void Logout(const string& username) {
+        const auto it = Username2Password.find(username);
+        const auto lit = LoggedInUsers.find(username);
+        if (it == Username2Password.end()) {
+            cout << "fail: no such user" << endl;
+        } else if (lit == LoggedInUsers.end()) {
+            cout << "fail: already logged out" << endl;
+        } else {
+            LoggedInUsers.erase(lit);
+            cout << "success: user logged out" << endl;
+        }
+    }
+};
+
+int main() {
+    TApplication application;
+    application.Run();
     return 0;
 }
