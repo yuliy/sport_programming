2e9cb0e4a7575d692cef905a3cf55e3a25762e2b
Yuliy Ayvazyan <yuliy@yandex-team.ru>
2016-02-06 20:50:57 +0300

    timus 2066 solution

--- timus/2066/main.cpp
+++ timus/2066/main.cpp
@@ -10,7 +10,38 @@
 #include <algorithm>
 using namespace std;
 
-int main() {
+int solveSigns(int a, int b, int c) {
+    return min({
+        a + b + c,
+        a + b - c,
+        a + b * c,
 
+        a - b + c,
+        a - b - c,
+        a - b * c,
+
+        a * b + c,
+        a * b - c,
+        a * b * c,
+    });
+}
+
+int solve(int a, int b, int c) {
+    return min({
+        solveSigns(a, b, c),
+        solveSigns(a, c, b),
+
+        solveSigns(b, a, c),
+        solveSigns(b, c, a),
+
+        solveSigns(c, a, b),
+        solveSigns(c, b, a)
+    });
+}
+
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+    cout << solve(a, b, c) << endl;
     return 0;
 }
