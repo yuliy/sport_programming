8ab908e749163eb9a113e0bdce316ae1fcfeadf7
Yuliy Ayvazyan <yuliy@yandex-team.ru>
2016-02-08 17:03:24 +0300

    timus 2035 solution

--- timus/2035/main.cpp
+++ timus/2035/main.cpp
@@ -10,7 +10,18 @@
 #include <algorithm>
 using namespace std;
 
+using i64 = long long;
+
 int main() {
+    i64 X, Y, C;
+    cin >> X >> Y >> C;
+
+    if ((X + Y) < C) {
+        cout << "Impossible" << endl;
+    } else {
+        const i64 A = min(X, C);
+        cout << A << ' ' << (C - A) << endl;
+    }
 
     return 0;
 }
