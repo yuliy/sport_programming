5cfb49d928a158b7b7bdc5f6fd9233716d987aea
Yuliy Ayvazyan <yuliy@yandex-team.ru>
2016-02-08 18:21:02 +0300

    timus 2011 solution

--- timus/2011/main.cpp
+++ timus/2011/main.cpp
@@ -11,6 +11,53 @@
 using namespace std;
 
 int main() {
+    int n, a, b, c;
+    cin >> n;
+
+    a = b = c = 0;
+    for (int i = 0; i < n; ++i) {
+        int tmp;
+        cin >> tmp;
+        if (tmp == 1)
+            ++a;
+        if (tmp == 2)
+            ++b;
+        if (tmp == 3)
+            ++c;
+    }
+
+    if (a > c)
+        swap(a, c);
+    if (a > b)
+        swap(a, b);
+    if (b > c)
+        swap(b, c);
+
+    if ((a != 0) && (b != 0) && (c != 0)) {
+        cout << "Yes" << endl;
+        return 0;
+    }
+
+    if (b == 0) {
+        cout << "No" << endl;
+        return 0;
+    }
+
+    if (b == 1) {
+        if (c >= 5) {
+            cout << "Yes" << endl;
+        } else {
+            cout << "No" << endl;
+        }
+        return 0;
+    }
+
+    if (c >= 2) {
+        cout << "Yes" << endl;
+        return 0;
+    }
+
+    cout << "No" << endl;
 
     return 0;
 }
