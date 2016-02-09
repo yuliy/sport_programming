63b3e1edb3421b69e3acf5dc0957669372efd738
Yuliy Ayvazyan <yuliy@yandex-team.ru>
2016-02-09 23:46:56 +0300

    timus 2020 solution

--- timus/2020/main.cpp
+++ timus/2020/main.cpp
@@ -11,6 +11,41 @@
 using namespace std;
 
 int main() {
+    string s1, s2;
+    cin >> s1 >> s2;
 
+    auto it1 = s1.begin();
+    auto it2 = s2.begin();
+    int res = 0;
+    for (; it1 != s1.end() || it2 != s2.end(); ++res) {
+        if (it1 == s1.end()) {
+            ++it2;
+            continue;
+        }
+
+        if (it2 == s2.end()) {
+            ++it1;
+            continue;
+        }
+
+        const char c1 = *it1;
+        const char c2 = *it2;
+
+        if (c1 == 'L' && c2 == 'L') {
+            ++it1;
+            ++it2;
+            continue;
+        }
+
+        if (c1 == 'R' || c1 == 'F') {
+            ++it1;
+        }
+
+        if (c2 == 'R' || c2 == 'F') {
+            ++it2;
+        }
+    }
+
+    cout << res << endl;
     return 0;
 }
