1a4350cecbddda7369c242b43d3bc88d2092c7ed
Yuliy Ayvazyan <yuliy@yandex-team.ru>
2016-02-09 23:18:59 +0300

    timus 2056 solution

--- timus/2056/main.cpp
+++ timus/2056/main.cpp
@@ -11,6 +11,37 @@
 using namespace std;
 
 int main() {
+    int n;
+    cin >> n;
 
+    int c3 = 0;
+    int c4 = 0;
+    int c5 = 0;
+    for (int i = 0; i < n; ++i) {
+        int tmp;
+        cin >> tmp;
+        if (tmp == 3) ++c3;
+        if (tmp == 4) ++c4;
+        if (tmp == 5) ++c5;
+    }
+
+    const double avg = (c3 * 3 + c4 * 4 + c5 * 5) * 1.0 / n;
+
+    if (c3 != 0) {
+        cout << "None" << endl;
+        return 0;
+    }
+
+    if (c5 == n) {
+        cout << "Named" << endl;
+        return 0;
+    }
+
+    if (avg >= 4.5) {
+        cout << "High" << endl;
+        return 0;
+    }
+
+    cout << "Common" << endl;
     return 0;
 }
