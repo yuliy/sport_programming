9db01dcfe5ea9424d93ed7244c613753fd227982
Yuliy Ayvazyan <yuliy@yandex-team.ru>
2016-02-09 23:12:20 +0300

    timus 2056 draft

---
+++ timus/2056/main.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <list>
+#include <algorithm>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
