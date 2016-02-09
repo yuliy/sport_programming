483fbd902954d94d122b53dece751be6b1fa8369
Yuliy Ayvazyan <yuliy@yandex-team.ru>
2016-02-09 19:20:08 +0300

    timus 2033 solution

--- timus/2033/main.cpp
+++ timus/2033/main.cpp
@@ -11,6 +11,45 @@
 using namespace std;
 
 int main() {
+    map<string, int> device2count;
+    map<string, int> device2cost;
+
+    for (int i = 0; i < 6; ++i) {
+        string name;
+        string device;
+        int cost = 0;
+        cin >> name >> device >> cost;
+
+        device2count[device] += 1;
+
+        const auto it = device2cost.find(device);
+        if (it != device2cost.end()) {
+            device2cost[device] = min(it->second, cost);
+        } else {
+            device2cost[device] = cost;
+        }
+    }
+
+    string bestDevice;
+    int bestDeviceCount = 0;
+    int bestDeviceCost = 1e9;
+    for (const auto& dc : device2count) {
+        const string& device = dc.first;
+        const int count = dc.second;
+        const int cost = device2cost[device];
+
+        if (
+            (count > bestDeviceCount)
+            ||
+            ((count == bestDeviceCount) && (cost < bestDeviceCost))
+        ) {
+            bestDevice = device;
+            bestDeviceCount = count;
+            bestDeviceCost = cost;
+        }
+    }
+
+    cout << bestDevice << endl;
 
     return 0;
 }
