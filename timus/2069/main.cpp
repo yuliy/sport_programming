745e284f1b24eb375059314b58445a6bbbe7294f
Yuliy Ayvazyan <yuliy@yandex-team.ru>
2016-02-11 18:31:07 +0300

    timus 2069 solution

--- timus/2069/main.cpp
+++ timus/2069/main.cpp
@@ -10,7 +10,40 @@
 #include <algorithm>
 using namespace std;
 
+int Max(const vector<int>& v) {
+    int res = -1e9;
+    for (int elem : v) {
+        res = max(res, elem);
+    }
+    return res;
+}
+
 int main() {
+    int n, m;
+    scanf("%d %d", &n, &m);
+
+    vector<int> v(n);
+    vector<int> h(m);
+    for (int i = 0; i < n; ++i) {
+        scanf("%d", &v[i]);
+    }
+    for (int i = 0; i < m; ++i) {
+        scanf("%d", &h[i]);
+    }
+
+    int res = max(
+        min(v[0], h[m-1]),
+        min(h[0], v[n-1])
+    );
+    res = max(
+        res,
+        min(h[0], min(h[m-1], Max(v)))
+    );
+    res = max(
+        res,
+        min(v[0], min(v[n-1], Max(h)))
+    );
+    cout << res << endl;
 
     return 0;
 }
