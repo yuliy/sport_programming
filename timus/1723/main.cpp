bc7233dc7c2eaf6267d3911c20ee960c701da394
Yuliy Ayvazyan <yuliy@yandex-team.ru>
2016-02-14 09:47:33 +0300

    timus 1723 solution

--- timus/1723/main.cpp
+++ timus/1723/main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <string.h>
 #include <map>
 #include <set>
 #include <unordered_map>
@@ -11,6 +12,25 @@
 using namespace std;
 
 int main() {
+    string s;
+    cin >> s;
 
+    const size_t sz = s.length();
+    unordered_map<char, int> ch2cnt;
+    for (size_t i = 0; i < sz; ++i) {
+        ++ch2cnt[s[i]];
+    }
+
+    char res = s[0];
+    int maxCnt = -1;
+
+    for (const auto& elem : ch2cnt) {
+        if (elem.second > maxCnt) {
+            res = elem .first;
+            maxCnt = elem.second;
+        }
+    }
+
+    cout << res << endl;
     return 0;
 }
