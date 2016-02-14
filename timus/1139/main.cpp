8d500caa0e1635ebed2136576e3c7bf11816bd7e
Yuliy Ayvazyan <yuliy@yandex-team.ru>
2016-02-14 13:00:08 +0300

    timus 1139 solution

--- timus/1139/main.cpp
+++ timus/1139/main.cpp
@@ -11,6 +11,25 @@
 using namespace std;
 
 int main() {
+    int N, M;
+    scanf("%d %d", &N, &M);
 
+    const double mult = 1.0 * (M - 1) / (N - 1);
+    const double epsilon = 1e-6;
+
+    double prevY = 0;
+    int res = 0;
+    for (double x = 1; x < N; ++x) {
+        const double nextY = mult * x;
+        const int py = floor(prevY);
+        const int ny = floor(nextY);
+        res += (ny - py);
+        if ((nextY - ny) > epsilon)
+            ++res;
+
+        prevY = nextY;
+    }
+
+    cout << res << endl;
     return 0;
 }
