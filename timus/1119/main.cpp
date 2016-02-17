143a1dbc8db51861e6c0ee1c83ac475ddf4f526d
Yuliy Ayvazyan <yuliy@yandex-team.ru>
2016-02-17 17:38:06 +0300

    @

---
+++ timus/1119/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <stdlib.h>
+#include <cmath>
+#include <stdio.h>
+#include <cmath>
+#include <set>
+using namespace std;
+
+int main() {
+    int N, M, K;
+    cin >> N >> M >> K;
+
+    for (int i = 0; i < K; ++i) {
+        int x, y;
+        scanf("%d %d", &x, &y);
+    }
+
+    return 0;
+}
