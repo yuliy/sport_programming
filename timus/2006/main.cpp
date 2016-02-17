ab2addfd4a3d46638e0fcda8317da7fed0712a05
Yuliy Ayvazyan <yuliy@yandex-team.ru>
2016-02-17 22:37:37 +0300

    @

--- timus/2006/main.cpp
+++ timus/2006/main.cpp
@@ -10,7 +10,20 @@
 #include <algorithm>
 using namespace std;
 
+bool EmptyCell[1001][1001] = {false};
+int Dist[1001][1001] = {0};
+
 int main() {
+    int N, M, py, px, hy, hx;
+    scanf("%d %d %d %d %d %d", &N, &M, &py, &px, &hy, &hx);
+
+    for (int i = 0; i < N; ++i) {
+        string s;
+        cin >> s;
+        for (int j = 0; j < M; ++j) {
+            EmptyCell[i][j] = s[i] == '.';
+        }
+    }
 
     return 0;
 }
