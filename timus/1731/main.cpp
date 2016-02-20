d2c1be46ce1291e44d8948c6c52f0db0c0348567
Yuliy Ayvazyan <yuliy@yandex-team.ru>
2016-02-20 12:45:58 +0300

    timus 1731 solution

--- timus/1731/main.cpp
+++ timus/1731/main.cpp
@@ -9,6 +9,18 @@
 using namespace std;
 
 int main() {
+    int n, m;
+    cin >> n >> m;
+
+    for (int i = 1; i <= n; ++i) {
+        cout << i << ' ';
+    }
+    cout << endl;
+
+    for (int i = 1; i <= m; ++i) {
+        cout << (i * n + 1) << ' ';
+    }
+    cout << endl;
 
     return 0;
 }
