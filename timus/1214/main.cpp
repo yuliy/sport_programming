2815765437fc63168ea8853db4b1cbd583fda431
Yuliy Ayvazyan <yuliy@yandex-team.ru>
2016-02-27 18:05:09 +0300

    timus 1214 solution

--- timus/1214/main.cpp
+++ timus/1214/main.cpp
@@ -6,15 +6,18 @@
 #include <set>
 #include <list>
 #include <algorithm>
+#include <utility>
 using namespace std;
 
 int main() {
-    long x, y;
+    long long x, y;
     cin >> x >> y;
-    if (x > 0 && y > 0) {
 
-    } else {
-        cout << x << ' ' << y << endl;
+    if (x > 0 && y > 0) {
+        if ((x + y) % 2)
+            swap(x, y);
     }
+
+    cout << x << ' ' << y << endl;
     return 0;
 }
