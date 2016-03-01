b10c73658f17e7f220adbf3e1198f11ac71a1b50
Yuliy Ayvazyan <yuliy@yandex-team.ru>
2016-03-02 02:26:46 +0300

    @

--- timus/1582/main.cpp
+++ timus/1582/main.cpp
@@ -10,8 +10,29 @@
 #include <algorithm>
 using namespace std;
 
+double CalcX2(double k1, double k2, double k3) {
+    const double num = (1 + k2) * (1 + k3) + (1 + k1) * (1 + k3) + (1 + k2) * (1 + k1);
+    const double den = (1 + k1) * (1 + k3);
+    return 1000 * den / num;
+}
+
 int main() {
     double k1, k2, k3;
     cin >> k1 >> k2 >> k3;
+
+
+    const double x2 = CalcX2(k1, k2, k3);
+    const double x1 = (1 + k2) * x2 / (1 + k1);
+    const double x3 = (1 + k2) * x2 / (1 + k3);
+
+    //const double P = k1 * x1 - x2 - x3;
+
+    const double res = min(k1*x1, min(k2*x2, k3*x3));
+    //cout << round(P) << endl;
+    cout << k1 * x1 << endl
+        << k2 * x2 << endl
+        << k3 * x3 << endl;
+    cout << res << endl;
+
     return 0;
 }
