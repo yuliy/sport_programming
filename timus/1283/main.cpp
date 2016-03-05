fc06a52e38458dfc83bef4fe1ab2531732d293ce
Yuliy Ayvazyan <yuliy@yandex-team.ru>
2016-03-05 07:49:52 +0300

    timus 1283 solution

--- timus/1283/main.cpp
+++ timus/1283/main.cpp
@@ -12,10 +12,18 @@
 using namespace std;
 
 int main() {
-    //n <= log[e](lim/sum) / log[e](k/100)
+    //n <= log[e](lim/sum) / log[e]((100-k)/100)
     double sum, lim, k;
     cin >> sum >> lim >> k;
-    const double n = log(lim/sum) / log(k/100);
-    cout << floor(n) << endl;
+
+    int res = 0;
+    const double m = (100-k) / 100;
+    while (sum > lim) {
+        sum *= m;
+        ++res;
+    }
+    cout << res << endl;
+    //const double n = log(lim/sum) / log((100-k)/100);
+    //cout << ceil(n) << endl;
     return 0;
 }
