1ca645563a5c4e271b67c89e4d4c9964dc0bcaa4
Yuliy Ayvazyan <yuliy@yandex-team.ru>
2016-03-05 07:36:17 +0300

    @

--- timus/1283/main.cpp
+++ timus/1283/main.cpp
@@ -8,9 +8,14 @@
 #include <unordered_set>
 #include <list>
 #include <algorithm>
+#include <cmath>
 using namespace std;
 
 int main() {
-
+    //n <= log[e](lim/sum) / log[e](k/100)
+    double sum, lim, k;
+    cin >> sum >> lim >> k;
+    const double n = log(lim/sum) / log(k/100);
+    cout << floor(n) << endl;
     return 0;
 }
