a30b40e681a58036c7136c698f8dcd5b4b14fcdb
Yuliy Ayvazyan <yuliy@yandex-team.ru>
2016-03-05 19:19:05 +0300

    timus 1688 solution

--- timus/1688/main.cpp
+++ timus/1688/main.cpp
@@ -11,6 +11,26 @@
 using namespace std;
 
 int main() {
+    long long n, m;
+    cin >> n >> m;
+
+    n *= 3;
+    long long sum = 0;
+    int times = 0;
+    for (int i = 0; i < m; ++i) {
+        long long tmp;
+        cin >> tmp;
+        sum += tmp;
+        ++times;
+        if (sum > n)
+            break;
+    }
+
+    if (sum > n) {
+        cout << "Free after " << times << " times." << endl;
+    } else {
+        cout << "Team.GOV!" << endl;
+    }
 
     return 0;
 }
