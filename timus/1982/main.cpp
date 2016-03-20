f38dff2132f2f8d0f2fd1ef255bee179daa99f9b
Yuliy Ayvazyan <yuliy@yandex-team.ru>
2016-03-20 16:13:57 +0300

    timus 1982 solution

--- timus/1982/main.cpp
+++ timus/1982/main.cpp
@@ -16,47 +16,70 @@ public:
     void Run() {
         Init();
 
-        deque<int> q;
-        for (auto city : ECities) {
-            q.push_back(city);
-        }
+        set<size_t> visitedVertices;
+        visitedVertices.insert(ECities.begin(), ECities.end());
 
-        deque<bool> visited(N);
-
-        for (; !q.empty();) {
-            const int v = q[0];
-            q.pop_front();
-            for (int i = 0; i < N; ++i) {
-                if (i == v)
+        multimap<size_t, pair<size_t, size_t>> edges;
+        for (auto from : ECities) {
+            for (size_t to = 0; to < N; ++to) {
+                if (from == to)
                     continue;
 
-                if (visited[i]) {
-
-                } else {
-
-                }
+                const size_t w = Graph[from][to];
+                edges.insert(make_pair(
+                    w,
+                    make_pair(from, to)
+                ));
             }
         }
+
+        size_t res = 0;
+
+        while (visitedVertices.size() < N) {
+            const auto edgeIter = edges.begin();
+            const size_t w = edgeIter->first;
+            //const size_t from = edgeIter->second.first;
+            const size_t to = edgeIter->second.second;
+            edges.erase(edgeIter);
+
+            if (visitedVertices.find(to) != visitedVertices.end()) {
+                visitedVertices.insert(to);
+                continue;
+            }
+
+            res += w;
+            visitedVertices.insert(to);
+            for (size_t nto = 0; nto < N; ++nto) {
+                const size_t nw = Graph[to][nto];
+                edges.insert(make_pair(
+                    nw,
+                    make_pair(to, nto)
+                ));
+            }
+
+        }
+
+        cout << res << endl;
     }
 private:
-    int N;
-    int K;
-    vector<int> ECities;
-    vector<vector<int>> Graph;
+    size_t N;
+    size_t K;
+    vector<size_t> ECities;
+    vector<vector<size_t>> Graph;
 private:
     void Init() {
         cin >> N >> K;
 
         ECities.resize(K);
-        for (int i = 0; i < K; ++i) {
+        for (size_t i = 0; i < K; ++i) {
             cin >> ECities[i];
             --ECities[i];
         }
 
         Graph.resize(N);
-        for (int i = 0; i < N; ++i) {
+        for (size_t i = 0; i < N; ++i) {
             Graph[i].resize(N);
-            for (int j = 0; j < N; ++j) {
+            for (size_t j = 0; j < N; ++j) {
                 cin >> Graph[i][j];
             }
         }
