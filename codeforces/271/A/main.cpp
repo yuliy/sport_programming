0812fe869d54bdc594b1cd8a98289d237861db8a
Yuliy Ayvazyan <yuliy@yandex-team.ru>
2016-06-07 20:27:11 +0300

    @

--- codeforces/271/A/main.cpp
+++ codeforces/271/A/main.cpp
@@ -10,7 +10,26 @@
 #include <algorithm>
 using namespace std;
 
-int main() {
+static bool IsMagic(int num) {
+    bool digits[10] = {false};
+    while (num) {
+        const int d = num % 10;
+        num /= 10;
+        if (digits[d])
+            return false;
+        digits[d] = true;
+    }
+    return true;
+}
 
+int main() {
+    int num;
+    cin >> num;
+    for (int i = num + 1; i <= 10000; ++i) {
+        if (IsMagic(i)) {
+            cout << i << endl;
+            return 0;
+        }
+    }
     return 0;
 }
