7ee2a75f0e5334d24d44f0cd3a437958e78bae22
Yuliy Ayvazyan <yuliy@yandex-team.ru>
2016-06-14 19:50:30 +0300

    @

--- codeforces/round357/B/main.cpp
+++ codeforces/round357/B/main.cpp
@@ -10,7 +10,15 @@
 #include <algorithm>
 using namespace std;
 
-int main() {
+using i64 = long long;
 
+bool Solve(i64 N) {
+    //
+}
+
+int main() {
+    i64 N;
+    cin >> N;
+    cout << (Solve(N) ? "YES" : "NO") << endl;
     return 0;
 }
