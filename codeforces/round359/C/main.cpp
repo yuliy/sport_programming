9b43294556041d9b3ad5b00c6ee6c48aff974081
Yuliy Ayvazyan <yuliy@yandex-team.ru>
2016-06-23 19:27:09 +0300

    @

---
+++ codeforces/round359/C/main.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <deque>
+#include <cstring>
+#include <string>
+#include <map>
+#include <set>
+#include <list>
+#include <algorithm>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
