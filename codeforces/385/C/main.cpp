b8bec3a555af331bd96852d597b66388e61e75d1
Yuliy Ayvazyan <yuliy@yandex-team.ru>
2016-06-28 20:32:35 +0300

    @

--- codeforces/385/C/main.cpp
+++ codeforces/385/C/main.cpp
@@ -10,7 +10,49 @@
 #include <algorithm>
 using namespace std;
 
-int main() {
+using i64 = long long;
 
+int N;
+vector<int> X;
+vector<int> A(1e7 + 1);
+vector<int> F(1e7 + 1);
+
+int main() {
+    scanf("%d", &N);
+    X = vector<int>(N);
+    for (int i = 0; i < N; ++i) {
+        scanf("%d", &X[i]);
+        ++A[ X[i] ];
+    }
+
+    vector<bool> sieve(1e7 + 1, true);
+    for (size_t i = 2; i < sieve.size(); ++i) {
+        if (sieve[i]) {
+            ++F[i];
+            for (size_t j = i * i; j < sieve.size(); j +=i) {
+                if (j > 1e7)
+                    break;
+
+                sieve[j] = false;
+                F[j] += A[j];
+            }
+        }
+    }
+
+    for (int i = 1; i <= 1e7; ++i) {
+        F[i] += F[i-1];
+    }
+
+    int M;
+    scanf("%d", &M);
+    for (int i = 0; i < M; ++i) {
+        int li, ri;
+        scanf("%d %d", &li, &ri);
+        if (ri > 1e7)
+            ri = 1e7;
+
+        const int res = F[ri] - F[li-1];
+        printf("%d\n", res);
+    }
     return 0;
 }
