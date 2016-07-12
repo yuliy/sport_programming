2d222496b1497c30f8076670d59853b13fd97abc
Yuliy Ayvazyan <yuliy@yandex-team.ru>
2016-07-12 20:05:39 +0300

    @

--- codeforces/631/A/main.cpp
+++ codeforces/631/A/main.cpp
@@ -10,7 +10,67 @@
 #include <algorithm>
 using namespace std;
 
+using ui32 = unsigned int;
+
+struct TNum {
+    int Arr[32] = {0};
+
+    void Add(ui32 num) {
+        for (int i = 0; i < 32; ++i) {
+            if (num & (1 << i)) {
+                ++Arr[i];
+            }
+        }
+    }
+
+    void Remove(ui32 num) {
+        for (int i = 0; i < 32; ++i) {
+            if ((num & (1 << i)) && (Arr[i] > 0)) {
+                --Arr[i];
+            }
+        }
+    }
+
+    ui32 Convert() const {
+        ui32 res = 0;
+        for (int i = 0; i < 32; ++i) {
+            if (Arr[i] > 0) {
+                res |= (1 << i);
+            }
+        }
+        return res;
+    }
+};
+
 int main() {
+    int N;
+    cin >> N;
+
+    vector<ui32> a(N);
+    for (int i = 0; i< N; ++i) {
+        cin >> a[i];
+    }
+
+    vector<ui32> b(N);
+    for (int i = 0; i < N; ++i) {
+        cin >> b[i];
+    }
+
+    ui32 res = 0;
+    for (int l = 0; l < N; ++l) {
+        TNum aNum;
+        TNum bNum;
+        aNum.Add(a[l]);
+        bNum.Add(b[l]);
+        res = max(res, aNum.Convert() + bNum.Convert());
+        for (int r = l + 1; r < N; ++r) {
+            aNum.Add(a[r]);
+            bNum.Add(b[r]);
+            res = max(res, aNum.Convert() + bNum.Convert());
+        }
+    }
+
+    cout << res << endl;
 
     return 0;
 }
