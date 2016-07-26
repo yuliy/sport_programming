41e16987770d4d427c06e051d20bce4a019e65f5
Yuliy Ayvazyan <yuliy@yandex-team.ru>
2016-07-26 20:12:13 +0300

    @

--- codeforces/652/B/main.cpp
+++ codeforces/652/B/main.cpp
@@ -11,6 +11,32 @@
 using namespace std;
 
 int main() {
+    int N;
+    scanf("%d", &N);
+    vector<int> odd, even;
+    for (int i = 0 ; i < N; ++i) {
+        int elem;
+        scanf("%d", &elem);
+        if (i & 1) {
+            odd.push_back(elem);
+        } else {
+            even.push_back(elem);
+        }
+    }
+
+    std::sort(even.begin(), even.end());
+    std::sort(odd.begin(), odd.end(), greater<int>());
+
+    int oddPos = 0;
+    int evenPos = 0;
+    for (int i = 0; i < N; ++i) {
+        if (i & 1) {
+            printf("%d ", odd[oddPos++]);
+        } else {
+            printf("%d ", even[evenPos++]);
+        }
+    }
+    printf("\n");
 
     return 0;
 }
