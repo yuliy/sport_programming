5a6e00d3563be5dff82827e00492e03f8dfa376a
Yuliy Ayvazyan <yuliy@yuliy.me>
2020-05-29 22:33:21 +0300

    @

--- codeforces/vcontest_640_div4/C/main.cpp
+++ codeforces/vcontest_640_div4/C/main.cpp
@@ -3,7 +3,11 @@
 using namespace std;
 
 int Solve(int n, int k) {
-    //
+    cout << "===============" << endl;
+    const int m = k / (n-1);
+    const int nk = m * (n-1);
+    const int ch = m * n;
+    return ch + (k - nk);
 }
 
 
