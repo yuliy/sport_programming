0303d7cc2e577cf2d3a457f1c7b65dedf71fc57c
Yuliy Ayvazyan <yuliy@yuliy.me>
2020-05-29 21:39:25 +0300

    @

--- codeforces/vcontest_640_div4/B/main.cpp
+++ codeforces/vcontest_640_div4/B/main.cpp
@@ -2,9 +2,22 @@
 #include <vector>
 using namespace std;
 
+void Solve(int n, int k) {
+    //
+}
+
 int main() {
     cin.tie(0);
     ios_base::sync_with_stdio(0);
+
+    int t;
+    cin >> t;
+
+    for (int i = 0; i < t; ++i) {
+        int n, k;
+        cin >> n >> k;
+        Solve(n, k);
+    }
 
     return 0;
 }
