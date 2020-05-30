894f99860d620fe8a1951f3aba8f35c80532ff8f
Yuliy Ayvazyan <yuliy@yuliy.me>
2020-05-31 00:01:24 +0300

    @

--- codeforces/339/A/main.cpp
+++ codeforces/339/A/main.cpp
@@ -11,8 +11,23 @@
 using namespace std;
 
 int main() {
-    cin.tie(0);
-    ios_base::sync_with_stdio(0);
+    string s;
+    cin >> s;
+    vector<char> v;
+    for (auto ch : s) {
+        if (ch != '+') {
+            v.push_back(ch);
+        }
+    }
+
+    sort(v.begin(), v.end());
+    const auto n = v.size();
+    for (int i = 0; i < n; ++i) {
+        cout << v[i];
+        if (i != (n-1))
+            cout << "+";
+    }
+    cout << endl;
 
     return 0;
 }
