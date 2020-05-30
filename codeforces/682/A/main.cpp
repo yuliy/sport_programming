ee4eee9e32cec0d31ab08a92e8713fdff12650de
Yuliy Ayvazyan <yuliy@yuliy.me>
2020-05-31 01:49:02 +0300

    @

--- codeforces/682/A/main.cpp
+++ codeforces/682/A/main.cpp
@@ -10,7 +10,37 @@
 #include <algorithm>
 using namespace std;
 
+using ui64 = unsigned long long;
+
 int main() {
+    cin.tie(0);
+    ios_base::sync_with_stdio(0);
+
+    ui64 n, m;
+    cin >> n >> m;
+
+    vector<ui64> vn(5);
+    vector<ui64> vm(5);
+
+    for (ui64 i = 1; i <= n; ++i) {
+        vn[i % 5] += 1;
+    }
+
+    for (ui64 i = 1; i <= m; ++i) {
+        vm[i % 5] += 1;
+    }
+
+    ui64 res = 0;
+    for (ui64 i = 0; i < 5; ++i) {
+        for (ui64 j = 0; j < 5; ++j) {
+            const ui64 tmp = (i+j) % 5;
+            if (0 == tmp) {
+                res += (vn[i] * vm[j]);
+            }
+        }
+    }
+
+    cout << res << endl;
 
     return 0;
 }
