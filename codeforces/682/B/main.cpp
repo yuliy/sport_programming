78772e7fc5aa47253839f2729422f9f477ae8f23
Yuliy Ayvazyan <yuliy@yuliy.me>
2020-05-31 02:18:30 +0300

    @

--- codeforces/682/B/main.cpp
+++ codeforces/682/B/main.cpp
@@ -14,5 +14,22 @@ int main() {
     cin.tie(0);
     ios_base::sync_with_stdio(0);
 
+    int n;
+    cin >> n;
+
+    vector<int> v(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> v[i];
+    }
+
+    sort(v.begin(), v.end());
+    v[0] = 1;
+    for (int i = 1; i < n; ++i) {
+        if (v[i-1] < v[i]) {
+            v[i] = v[i-1] + 1;
+        }
+    }
+
+    cout << (v.back() + 1) << endl;
     return 0;
 }
