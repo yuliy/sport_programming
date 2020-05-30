0c8b9a5449d8b018fc61ab39a97691cf0201d423
Yuliy Ayvazyan <yuliy@yuliy.me>
2020-05-30 23:41:56 +0300

    @

--- codeforces/263/A/main.cpp
+++ codeforces/263/A/main.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <deque>
 #include <cstring>
+#include <cstdlib>
 #include <string>
 #include <map>
 #include <set>
@@ -10,9 +11,24 @@
 #include <algorithm>
 using namespace std;
 
+void findIJ(int& i, int& j) {
+    for (i = 0; i < 5; ++i) {
+        for (j = 0; j < 5; ++j) {
+            int tmp;
+            cin >> tmp;
+            if (tmp == 1) {
+                return;
+            }
+        }
+    }
+}
+
 int main() {
     cin.tie(0);
     ios_base::sync_with_stdio(0);
 
+    int i, j;
+    findIJ(i, j);
+    cout << (abs(i - 2) + abs(j - 2)) << endl;
     return 0;
 }
