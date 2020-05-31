2ead1f633e03956344b9351594239ba4534f6a76
Yuliy Ayvazyan <yuliy@yuliy.me>
2020-05-31 18:59:01 +0300

    @

--- codeforces/677/A/main.cpp
+++ codeforces/677/A/main.cpp
@@ -14,5 +14,16 @@ int main() {
     cin.tie(0);
     ios_base::sync_with_stdio(0);
 
+    int n, h;
+    cin >> n >> h;
+
+    int res = 0;
+    for (int i = 0; i < n; ++i) {
+        int ai;
+        cin >> ai;
+        res += (ai > h) ? 2 : 1;
+    }
+
+    cout << res << endl;
     return 0;
 }
