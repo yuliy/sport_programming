0baa6a4b936e132f1805611187c2c57dd4bdecf0
Yuliy Ayvazyan <yuliy@yuliy.me>
2020-05-31 18:59:43 +0300

    @

---
+++ codeforces/677/B/main.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <deque>
+#include <cstring>
+#include <string>
+#include <map>
+#include <set>
+#include <list>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    cin.tie(0);
+    ios_base::sync_with_stdio(0);
+
+    return 0;
+}
