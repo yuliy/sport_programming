410189fff7b10bfe2374f6e6770c0511b65e710a
Yuliy Ayvazyan <yuliy@yuliy.me>
2020-06-05 20:56:06 +0300

    @

--- codeforces/1362/B/main.cpp
+++ codeforces/1362/B/main.cpp
@@ -13,9 +13,38 @@ using namespace std;
 using ui64 = unsigned long long;
 using i64 = long long;
 
+int Solve(int n, vector<int>& s) {
+    sort(s.begin(), s.end());
+    vector<int> sn(n);
+    for (int k = 1; k <= 1024; ++k) {
+        for (int i = 0; i < n; ++i) {
+            sn[i] = s[i] ^ k;
+        }
+        sort(sn.begin(), sn.end());
+        if (s == sn) {
+            return k;
+        }
+    }
+    return -1;
+}
+
 int main() {
     cin.tie(0);
     ios_base::sync_with_stdio(0);
+
+    int t = 0;
+    cin >> t;
+    for (int i = 0; i < t; ++i) {
+        int n;
+        cin >> n;
+        vector<int> s;
+        for (int j = 0; j < n; ++j) {
+            int tmp;
+            cin >> tmp;
+            s.push_back(tmp);
+        }
+        cout << Solve(n, s) << endl;
+    }
 
     return 0;
 }
