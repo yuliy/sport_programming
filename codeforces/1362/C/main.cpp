b8c8ed92c679f5b404fc8eb22b812dba7da0c0d5
Yuliy Ayvazyan <yuliy@yuliy.me>
2020-06-05 21:11:03 +0300

    @

--- codeforces/1362/C/main.cpp
+++ codeforces/1362/C/main.cpp
@@ -13,9 +13,27 @@ using namespace std;
 using ui64 = unsigned long long;
 using i64 = long long;
 
+i64 Solve(i64 n) {
+    i64 res = 0;
+    i64 mult = 1;
+    while (n / mult) {
+        res += (n/mult);
+        mult <<= 1;
+    }
+    return res;
+}
+
 int main() {
     cin.tie(0);
     ios_base::sync_with_stdio(0);
+
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; ++i) {
+        i64 n;
+        cin >> n;
+        cout << Solve(n) << endl;
+    }
 
     return 0;
 }
