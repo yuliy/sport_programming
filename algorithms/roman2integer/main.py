e0c6a1c8fa407a6b965776345e235aa74e52ea4a
Yuliy Ayvazyan <yuliy@yuliy.me>
2020-12-20 01:40:22 +0300

    @

--- algorithms/roman2integer/main.py
+++ algorithms/roman2integer/main.py
@@ -6,85 +6,27 @@
 #
 
 class Solution:
-    def Solution(self):
-        self.res = 0
-        self.s = None
-
-    def applyRules(self, rules):
-        for rule in rules:
-            prefix, value = rule
-            if self.s.startswith(prefix):
-                self.res += value
-                prefixLen = len(prefix)
-                self.s = self.s[prefixLen:]
-                break
-
     def romanToInt(self, s: str) -> int:
-        self.res = 0
-        self.s = s
-
-        self.applyRules((
-            ('MMMCM', 3900),
-            ('MMCM',  2900),
-            ('MMM',   3000),
-            ('MCM',   1900),
-            ('MM',    2000),
-            ('CM',     900),
-            ('M',     1000),
-        ))
-
-        self.applyRules((
-            ('DCCC', 800),
-            ('DCC',  700),
-            ('DC',   600),
-            ('CD',   400),
-            ('D',    500),
-        ))
-
-        self.applyRules((
-            ('CCCXC', 390),
-            ('CCC',   300),
-            ('CCXC',  290),
-            ('CC',    200),
-            ('CXC',   190),
-            ('C',     100),
-            ('XC',     90),
-        ))
-
-        self.applyRules((
-            ('LXXX', 80),
-            ('LXX',  70),
-            ('LX',   60),
-            ('XL',   40),
-            ('L',    50),
-        ))
-
-        self.applyRules((
-            ('XXX', 30),
-            ('XX',  20),
-        ))
-
-        d = {
-            'XIX':   19,
-            'XVIII': 18,
-            'XVII':  17,
-            'XVI':   16,
-            'XV':    15,
-            'XIV':   14,
-            'XIII':  13,
-            'XII':   12,
-            'XI':    11,
-            'X':     10,
-            'IX':     9,
-            'VIII':   8,
-            'VII':    7,
-            'VI':     6,
-            'V':      5,
-            'IV':     4,
-            'III':    3,
-            'II':     2,
-            'I':      1,
-            '':       0,
+        s2r = {
+            "I":1,
+            "V":5,
+            "X":10,
+            "L":50,
+            "C":100,
+            "D":500,
+            "M":1000,
         }
+        r = [s2r[ch] for ch in s]
 
-        return self.res + d[self.s]
+        if len(r)==1:
+            return r[0]
+
+        result = 0
+        for i in range(len(s)-1):
+            if r[i+1] > r[i]:
+                result -= r[i]
+            else:
+                result += r[i]
+
+        result += r[-1]
+        return result
