67acedd8e18e1a524b912f15524fafff500898db
Yuliy Ayvazyan <yuliy.ayvazyan@aliexpress.ru>
2021-10-01 20:13:47 +0300

    996A solution

--- codeforces/996/A/main.cpp
+++ codeforces/996/A/main.cpp
@@ -17,5 +17,20 @@ int main() {
     cin.tie(0);
     ios_base::sync_with_stdio(0);
 
+    int N;
+    cin >> N;
+
+    int res = 0;
+
+    for (auto p : {100, 20, 10, 5, 1})
+    {
+        const auto d = N / p;
+        res += d;
+        N -= (d * p);
+        //cout << "p: " << p << "\td: " << d << endl;
+    }
+
+    cout << res << endl;
+
     return 0;
 }
