24854193fed1b131b4166837bdf3cd21309bbd7a
Yuliy Ayvazyan <yuliy.ayvazyan@aliexpress.ru>
2021-10-14 17:21:24 +0300

    @

--- codeforces/vcontest_AliTestMeshup/A/main.cpp
+++ codeforces/vcontest_AliTestMeshup/A/main.cpp
@@ -13,9 +13,32 @@ using namespace std;
 using ui64 = unsigned long long;
 using i64 = long long;
 
+int Weight(char ch) {
+    return ch - 'a' + 1;
+}
+
 int main() {
     cin.tie(0);
     ios_base::sync_with_stdio(0);
+
+    int n, q;
+    string s;
+    cin >> n >> q >> s;
+
+    vector<i64> v(n+1);
+    v[0] = 0;
+    for (int i = 0; i <= n; ++i) {
+        const char ch = s[i];
+        v[i+1] = v[i] + Weight(ch);
+    }
+
+    for (int i = 0; i < q; ++i) {
+        int l, r;
+        cin >> l >> r;
+        const auto res = v[r] - v[l-1];
+        cout << res << endl;
+    }
+
 
     return 0;
 }
