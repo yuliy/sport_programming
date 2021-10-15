4d697cf959230585d70fab93d97c04f05e0a4497
Yuliy Ayvazyan <yuliy.ayvazyan@aliexpress.ru>
2021-10-15 16:11:31 +0300

    @

--- codeforces/1593/E/main.cpp
+++ codeforces/1593/E/main.cpp
@@ -13,9 +13,34 @@ using namespace std;
 using ui64 = unsigned long long;
 using i64 = long long;
 
+using TAdjList = vector<int>;
+using TGraph = vector<TAdjList>;
+
+int Solve(const TGraph& graph, int n) {
+
+}
+
 int main() {
     cin.tie(0);
     ios_base::sync_with_stdio(0);
+
+    int T;
+    cin >> T;
+    for (int t = 0; t < T; ++t) {
+        int n, k;
+        cin >> n >> k;
+        TGraph graph(n-1);
+        for (int i = 0; i < (n-1); ++i) {
+            int u, v;
+            cin >> u >> v;
+            --u;
+            --v;
+            graph[u].push_back(v);
+            graph[v].push_back(u);
+        }
+
+        cout << Solve(graph, n) << endl;
+    }
 
     return 0;
 }
