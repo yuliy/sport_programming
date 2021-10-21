ba963af0cd21e7c9a67f1d7e650709f47b93297c
Yuliy Ayvazyan <yuliy.ayvazyan@aliexpress.ru>
2021-10-22 01:22:46 +0300

    solved

--- codeforces/058/A/main.cpp
+++ codeforces/058/A/main.cpp
@@ -17,5 +17,17 @@ int main() {
     cin.tie(0);
     ios_base::sync_with_stdio(0);
 
+    std::string h = "hello";
+    std::string s;
+    cin >> s;
+
+    while (h.size() && s.size()) {
+        if (h.back() == s.back())
+            h.pop_back();
+        s.pop_back();
+    }
+
+    cout << (h.empty() ? "YES" : "NO") << endl;
+
     return 0;
 }
