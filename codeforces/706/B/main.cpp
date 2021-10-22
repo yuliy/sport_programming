b9e6fda3b6a528fc073b847427abeadb25438b3a
Yuliy Ayvazyan <yuliy.ayvazyan@aliexpress.ru>
2021-10-22 13:43:51 +0300

    solved

--- codeforces/706/B/main.cpp
+++ codeforces/706/B/main.cpp
@@ -13,9 +13,27 @@ using namespace std;
 using ui64 = unsigned long long;
 using i64 = long long;
 
+static int Solve(int n, int mi, const vector<int>& x) {
+    const auto it = upper_bound(x.begin(), x.end(), mi);
+    return it - x.begin();
+}
+
 int main() {
     cin.tie(0);
     ios_base::sync_with_stdio(0);
+
+    int n, q, mi;
+    cin >> n;
+    vector<int> x(n);
+    for (int i = 0; i < n; ++i)
+        cin >> x[i];
+    std::sort(x.begin(), x.end());
+
+    cin >> q;
+    for (int i = 0; i < q; ++i) {
+        cin >> mi;
+        cout << Solve(n, mi, x) << endl;
+    }
 
     return 0;
 }
