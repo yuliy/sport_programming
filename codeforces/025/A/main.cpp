8b225ae99c338c82d84530b72b5dbcd1f71061f3
Yuliy Ayvazyan <yuliy.ayvazyan@aliexpress.ru>
2021-10-25 01:38:50 +0300

    solved

--- codeforces/025/A/main.cpp
+++ codeforces/025/A/main.cpp
@@ -17,5 +17,27 @@ int main() {
     cin.tie(0);
     ios_base::sync_with_stdio(0);
 
+    int n;
+    cin >> n;
+
+    int evenIdx = -1;
+    int oddIdx = -1;
+    int evenCnt = 0;
+    int oddCnt = 0;
+    for (int i = 1; i <= n; ++i) {
+        int num;
+        cin >> num;
+        if (num & 1) {
+            oddIdx = i;
+            ++oddCnt;
+        } else {
+            evenIdx = i;
+            ++evenCnt;
+        }
+    }
+
+    const int res = (evenCnt == 1) ? evenIdx : oddIdx;
+    cout << res << endl;
+
     return 0;
 }
