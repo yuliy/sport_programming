06f3f8129ff0ab2929dd1951ad31a8ec5296371e
Yuliy Ayvazyan <yuliy.ayvazyan@aliexpress.ru>
2021-10-25 01:24:37 +0300

    solved

--- codeforces/492/B/main.cpp
+++ codeforces/492/B/main.cpp
@@ -17,5 +17,22 @@ int main() {
     cin.tie(0);
     ios_base::sync_with_stdio(0);
 
+    int n, l;
+    cin >> n >> l;
+
+    vector<int> v(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> v[i];
+    }
+
+    sort(v.begin(), v.end());
+
+    double res = max(v[0], l-v[n-1]);
+    for (int i = 1; i < n; ++i) {
+        const double tmp = 0.5 * (v[i] - v[i-1]);
+        res = max(res, tmp);
+    }
+
+    printf("%f\n", res);
     return 0;
 }
