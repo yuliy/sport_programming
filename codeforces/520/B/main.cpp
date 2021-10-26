a8040e4cf7e3aa84642fa0ede344580d717dd6a3
Yuliy Ayvazyan <yuliy.ayvazyan@aliexpress.ru>
2021-10-26 15:09:33 +0300

    remove junk code

--- codeforces/520/B/main.cpp
+++ codeforces/520/B/main.cpp
@@ -16,20 +16,16 @@ using TAdj = vector<int>;
 using TGraph = vector<TAdj>;
 
 int CalcDistance(int n, int m, int N, const TGraph& g) {
-    //cout << "n=" << n << " m=" << m << " N=" << N << endl;
     vector<int> discovered(N+1);
     vector<int> distance(N+1);
     deque<int> q;
     q.push_back(n);
     discovered[n] = 1;
     while (false == q.empty()) {
-        //cout << "________________________" << endl;
         const int v = q.front();
-        //cout << "v: " << v << endl;
         q.pop_front();
         const TAdj& adj = g[v];
         for (const auto u : adj) {
-            //cout << "u: " << u << endl;
             if (false == discovered[u]) {
                 discovered[u] = 1;
                 distance[u] = distance[v] + 1;
