fb949e64566f898649a9a09da4cd92566fe0883f
Yuliy Ayvazyan <yuliy.ayvazyan@aliexpress.ru>
2021-10-31 00:38:42 +0300

    solved

--- codeforces/479/C/main.cpp
+++ codeforces/479/C/main.cpp
@@ -17,5 +17,23 @@ int main() {
     cin.tie(0);
     ios_base::sync_with_stdio(0);
 
+    int n;
+    cin >> n;
+    vector< pair<int, int> > v(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> v[i].first >> v[i].second;
+    }
+    sort(v.begin(), v.end());
+
+    int prev = 0;
+    for (const auto& elem : v) {
+        const int ai = elem.first;
+        const int bi = elem.second;
+        const int next = (prev <= bi) ? bi : ai;
+        prev = next;
+    }
+
+    cout << prev << endl;
+
     return 0;
 }
