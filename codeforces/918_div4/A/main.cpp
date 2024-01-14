1e35283df7523e49d3343a05e18b22c711c461d6
Yuliy Ayvazyan <yuliy.ayvazyan@aliexpress.ru>
2024-01-14 13:58:39 +0300

    update

---
+++ codeforces/918_div4/A/main.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <deque>
+#include <cstring>
+#include <string>
+#include <map>
+#include <set>
+#include <list>
+#include <algorithm>
+using namespace std;
+
+using ui64 = unsigned long long;
+using i64 = long long;
+
+int main() {
+    cin.tie(0);
+    ios_base::sync_with_stdio(0);
+
+    return 0;
+}
