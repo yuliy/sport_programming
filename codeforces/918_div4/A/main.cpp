fb61b58a8e46b8d4617f3ca53194e1ba06d6468a
Yuliy Ayvazyan <yuliy.ayvazyan@aliexpress.ru>
2024-01-14 14:03:57 +0300

    update

--- codeforces/918_div4/A/main.cpp
+++ codeforces/918_div4/A/main.cpp
@@ -17,5 +17,13 @@ int main() {
     cin.tie(0);
     ios_base::sync_with_stdio(0);
 
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; ++i) {
+        int a, b, c;
+        cin >> a >> b >> c;
+        cout << (a ^ b ^ c) << endl;
+    }
+
     return 0;
 }
