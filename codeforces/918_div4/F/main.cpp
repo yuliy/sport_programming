1581af12c538b775fae770c91dc9a5fdbd3fa26d
Yuliy Ayvazyan <yuliy.ayvazyan@aliexpress.ru>
2024-01-28 00:39:35 +0300

    update

--- codeforces/918_div4/F/main.cpp
+++ codeforces/918_div4/F/main.cpp
@@ -11,6 +11,17 @@
 #include <map>
 using namespace std;
 
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+typedef __gnu_pbds::tree< \
+        int, __gnu_pbds::null_type, \
+        less<int>, \
+        __gnu_pbds::rb_tree_tag, \
+        __gnu_pbds::tree_order_statistics_node_update \
+    > \
+    ordered_set;
+
 using ui64 = unsigned long long;
 using i64 = long long;
 using Segment = pair<i64, i64>;
@@ -19,25 +30,20 @@ using Segment = pair<i64, i64>;
 i64 Solve() {
     int n;
     cin >> n;
-    vector< pair<i64, i64> > v;
-    for (int i = 0; i < n; ++i) {
-        cin >> v[i].first >> v[i].second;
+    vector< pair<i64, i64> > v(n);
+    for (int i = 0; i < n; ++i)
+        cin >> v[i].second >> v[i].first;
+    sort(v.begin(), v.end());
+
+    ordered_set s;
+    i64 res = 0;
+    for (auto [bi, ai] : v) {
+        // s.order_of_key(ai) - Number of items strictly smaller than ai
+        res += s.size() - s.order_of_key(ai);
+        s.insert(ai);
     }
 
-    sort(
-        v.begin(), v.end(),
-        [](const Segment& lhs, const Segment& rhs)
-        { return lhs.first < rhs.first; }
-    );
-
-    set<Segment> s(
-        [](const Segment& lhs, const Segment& rhs)
-        { return lhs.second < rhs.second; }
-    );
-
-    for (const auto &segment : v) {
-
-    }
+    return res;
 }
 
 int main() {
