25934e46e8b24e97f0107833aef7c64405c4673a
Yuliy Ayvazyan <yuliy.ayvazyan@aliexpress.ru>
2024-01-28 15:43:41 +0300

    update

--- codeforces/920_div3/A/main.cpp
+++ codeforces/920_div3/A/main.cpp
@@ -8,6 +8,7 @@
 #include <set>
 #include <list>
 #include <algorithm>
+#include <utility>
 using namespace std;
 
 using ui64 = unsigned long long;
@@ -20,6 +21,13 @@ int main() {
     int t;
     cin >> t;
     for (int i = 0; i < t; ++i) {
+        vector<pair<int, int>> v(4);
+        for (int j = 0; j < 4; ++j)
+            cin >> v[j].first >> v[j].second;
+        sort(v.begin(), v.end());
+
+        const int a = v[0].second - v[1].second;
+        cout << (a*a) << endl;
     }
 
     return 0;
