234353bafe7e31da265e184d0444b8a71b1a07d4
Yuliy Ayvazyan <yuliy.ayvazyan@aliexpress.ru>
2024-01-28 17:13:10 +0300

    update

--- codeforces/920_div3/C/main.cpp
+++ codeforces/920_div3/C/main.cpp
@@ -14,6 +14,12 @@ using namespace std;
 using ui64 = unsigned long long;
 using i64 = long long;
 
+
+bool Solve(i64 n, i64 f, i64 a, i64 b, const vector<i64> &m) {
+    //
+}
+
+
 int main() {
     cin.tie(0);
     ios_base::sync_with_stdio(0);
@@ -21,6 +27,12 @@ int main() {
     int t;
     cin >> t;
     for (int i = 0; i < t; ++i) {
+        i64 n, f, a, b;
+        cin >> n >> f >> a >> b;
+        vector<i64> m(n);
+        for (int j = 0; j < n; ++j)
+            cin >> m[j];
+        cout << (Solve(n, f, a, b, m) ? "YES" : "NO") << endl;
     }
 
     return 0;
