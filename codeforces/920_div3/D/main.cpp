0cc7ac6bc3fa0009dff72f5b7c65f42f1d8b8d4b
Yuliy Ayvazyan <yuliy.ayvazyan@aliexpress.ru>
2024-02-02 20:34:38 +0300

    update

--- codeforces/920_div3/D/main.cpp
+++ codeforces/920_div3/D/main.cpp
@@ -22,6 +22,31 @@ int main() {
     int t;
     cin >> t;
     for (int i = 0; i < t; ++i) {
+        i64 n, m;
+        cin >> n >> m;
+        vector<i64> a(n);
+        vector<i64> b(m);
+        for (int i = 0; i < n; ++i)
+            cin >> a[i];
+        for (int i = 0; i < m; ++i)
+            cin >> b[i];
+
+        sort(a.begin(), a.end());
+        sort(b.rbegin(), b.rend());
+
+        i64 res = 0;
+        for (int i=0, j=0; (i+j) < n; ) {
+            const i64 lsum = abs(a[i] - b[i]);
+            const i64 rsum = abs(a[n-j-1] - b[m-j-1]);
+            if (lsum >= rsum) {
+                res += lsum;
+                ++i;
+            } else {
+                res += rsum;
+                ++j;
+            }
+        }
+        cout << res << endl;
     }
 
     return 0;
