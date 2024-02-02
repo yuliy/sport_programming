ccd490fcaf0512080d8701365365735060a9744e
Yuliy Ayvazyan <yuliy.ayvazyan@aliexpress.ru>
2024-02-03 00:51:54 +0300

    update

---
+++ codeforces/920_div3/E/main.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <deque>
+#include <cstring>
+#include <string>
+#include <map>
+#include <set>
+#include <list>
+#include <algorithm>
+#include <utility>
+using namespace std;
+
+using ui64 = unsigned long long;
+using i64 = long long;
+
+
+int main() {
+    cin.tie(0);
+    ios_base::sync_with_stdio(0);
+
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; ++i) {
+    }
+
+    return 0;
+}
+
