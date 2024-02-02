0feed624b9425220936f21c0bb5f801f081b375b
Yuliy Ayvazyan <yuliy.ayvazyan@aliexpress.ru>
2024-02-03 01:14:23 +0300

    update

--- codeforces/920_div3/E/main.cpp
+++ codeforces/920_div3/E/main.cpp
@@ -14,6 +14,46 @@ using namespace std;
 using ui64 = unsigned long long;
 using i64 = long long;
 
+string Solve() {
+    i64 h, w, xa, ya, xb, yb;
+    cin >> h >> w >> xa >> ya >> xb >> yb;
+
+    cout << "_________________________" << endl;
+    cout
+        << "h=" << h << " w=" << w
+        << " xa=" << xa << " ya=" << ya
+        << " xb=" << xb << " yb=" << yb
+        << endl;
+
+    if (xa >= xb)
+        return "draw";
+
+    const i64 dy = abs(ya - yb);
+    const i64 dx = abs(xa - xb);
+    if (dy > 1)
+        return "draw";
+
+    const bool even = (dx & 1) == 0;
+    if (dy == 1) {
+        if (even) {
+            if ((ya < yb) && (yb < w))
+                return "draw";
+            if ((ya > yb) && (yb > 1))
+                return "draw";
+            return "alice";
+        } else {
+            if ((ya < yb) && (ya > 1))
+                return "draw";
+            if ((ya > yb) && (ya < w))
+                return "draw";
+            return "bob";
+        }
+    }
+
+    //if (dy == 0)
+    return even ? "alice" : "bob";
+}
+
 
 int main() {
     cin.tie(0);
@@ -22,6 +62,7 @@ int main() {
     int t;
     cin >> t;
     for (int i = 0; i < t; ++i) {
+        cout << Solve() << endl;
     }
 
     return 0;
