c39cc7eaa10a8bfc41161fafed39f350e1d78715
Yuliy Ayvazyan <yuliy.ayvazyan@aliexpress.ru>
2024-02-05 02:07:24 +0300

    update

--- codeforces/921_div2/A/main.cpp
+++ codeforces/921_div2/A/main.cpp
@@ -18,9 +18,19 @@ int main() {
     cin.tie(0);
     ios_base::sync_with_stdio(0);
 
+    const string alpha = "abcdefghijklmnopqrstuvwxyz";
+
     int t;
     cin >> t;
     for (int i = 0; i < t; ++i) {
+        int n, k;
+        cin >> n >> k;
+
+        for (int j = 0; j < n; ++j)
+            for (int p = 0; p < k; ++p)
+                cout << alpha[p];
+
+        cout << endl;
     }
 
     return 0;
