f4667d1905844c35fc9d84318935b8b0cdb38f08
Yuliy Ayvazyan <yuliy.ayvazyan@aliexpress.ru>
2025-08-12 00:57:02 +0300

    update

--- leetcode/3646/main.cpp
+++ leetcode/3646/main.cpp
@@ -11,8 +11,65 @@
 
 using namespace std;
 
+vector<int> digits;
 
-int main() {
 
+static void CalcDigits(uint64_t num)
+{
+    digits.clear();
+    while (num) {
+        digits.push_back(num % 10);
+        num /= 10;
+    }
+}
+
+
+static bool IsPalindrome()
+{
+    for (int i = 0; j = digits.size() - 1; i < j; ++i, --j)
+    {
+        if (digits[i] != digits[j])
+            return false;
+    }
+    return true;
+}
+
+
+static bool CheckDigitsRule()
+{
+    array<int, 10> digit2cnt;
+    for (int i = 0; i < digits.size(); ++i)
+    {
+        ++digit2cnt[i];
+    }
+
+    for (int i = 0; i < 10; ++i)
+    {
+        if (digit2cnt[i] != i)
+            return false;
+    }
+
+    return true;
+}
+
+
+static bool IsSpecial(uint64_t num)
+{
+    CalcDigits(num);
+    return IsPalindrome() && CheckDigitsRule();
+}
+
+
+uint64_t main() {
+    int special_numbers_found = 0;
+    for (uint64_t n = 0; n < 1e16; ++n)
+    {
+        if (IsSpecial(n))
+        {
+            ++special_numbers_found;
+            cout << "Special number found: " << n << " (total found: " << special_numbers_found << ")" << endl;
+        }
+    }
     return 0;
 }
+
